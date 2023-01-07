ab27c2ad112b4de9ef7c59587a96533aab8b2711
Admer <49600504+Admer456@users.noreply.github.com>
2023-01-07 23:08:08 +0100

    Add PluginLoadContext

---
+++ Plugins/PluginLoadContext.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Runtime.Loader;
+
+namespace Elegy
+{
+	internal class PluginLoadContext : AssemblyLoadContext
+	{
+		protected override Assembly? Load( AssemblyName assemblyName )
+		{
+			// Plugins are likely to be using these primarily
+			switch ( assemblyName.Name )
+			{
+				case "GodotSharp": return typeof( GD ).Assembly;
+				case "Elegy.Engine": return typeof( Engine ).Assembly;
+			}
+
+			// Check for any shared dependencies
+			AssemblyLoadContext? currentContext = GetLoadContext( typeof( Engine ).Assembly );
+			if ( currentContext == null )
+			{
+				return null;
+			}
+
+			foreach ( var assembly in currentContext.Assemblies )
+			{
+				if ( assembly.IsDynamic )
+				{
+					continue;
+				}
+
+				AssemblyName otherAssemblyName = new( assembly.FullName );
+				if ( assemblyName.Name == otherAssemblyName.Name )
+				{
+					return assembly;
+				}	
+			}
+
+			// Fallback to the host context
+			return null;
+		}
+	}
+}
