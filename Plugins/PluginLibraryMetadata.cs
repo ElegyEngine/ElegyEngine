88ca729b4cd17db657be2fcf50d7f6db3ac1ca34
Admer <49600504+Admer456@users.noreply.github.com>
2023-01-07 23:07:01 +0100

    Add PluginLibraryMetadata

---
+++ Plugins/PluginLibraryMetadata.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Elegy
+{
+	public class PluginLibraryMetadata
+	{
+		public PluginLibraryMetadata( string name, string description, string author, DateTime versionDate, string implementedInterface )
+		{
+			Name = name;
+			Description = description;
+			Author = author;
+			VersionDate = versionDate;
+			ImplementedInterface = implementedInterface;
+
+			ParsedCorrectly = true;
+		}
+
+		public string Name { get; } = string.Empty;
+		public string Description { get; } = string.Empty;
+		public string Author { get; } = string.Empty;
+		public DateTime VersionDate { get; }
+		public string ImplementedInterface { get; } = string.Empty;
+		public bool ParsedCorrectly { get; } = false;
+	}
+}
