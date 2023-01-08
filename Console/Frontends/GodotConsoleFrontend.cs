1392b0715f1cfd7b6fa844559b3499d6f01818d2
Admer <49600504+Admer456@users.noreply.github.com>
2023-01-08 21:22:51 +0100

    GodotConsoleFrontend: ignore \r; colours

--- Console/Frontends/GodotConsoleFrontend.cs
+++ Console/Frontends/GodotConsoleFrontend.cs
@@ -4,9 +4,18 @@ namespace Elegy.ConsoleFrontends
 	internal class GodotConsoleFrontend : IConsoleFrontend
 	{
 		public string Name => "Godot Terminal Console";
+		public string Error => string.Empty;
+		public bool Initialised { get; private set; } = false;
 
-		public void Init()
+		public bool Init()
 		{
+			Initialised = true;
+			return true;
+		}
+
+		public void Shutdown()
+		{
+			Initialised = false;
 		}
 
 		public void OnLog( ref ConsoleMessage message )
@@ -14,33 +23,37 @@ namespace Elegy.ConsoleFrontends
 			switch ( message.Type )
 			{
 				case ConsoleMessageType.Info:
+
+					message.Text = message.Text.Replace( "\r", string.Empty );
 					if ( message.Text.EndsWith( '\n' ) )
 					{
 						message.Text = message.Text.TrimEnd( '\n' );
 					}
+
 					GD.Print( message.Text );
 					break;
 
 				case ConsoleMessageType.Warning:
-					GD.Print( $"WARNING: {message.Text}" );
+					System.Console.ForegroundColor = ConsoleColor.Yellow;
+					GD.Print( $"[WARNING] {message.Text}" );
+					System.Console.ResetColor();
 					break;
 
 				case ConsoleMessageType.Error:
-					GD.PrintErr( $"ERROR: {message.Text}" );
+					System.Console.ForegroundColor = ConsoleColor.Red;
+					GD.PrintErr( $"[ERROR] {message.Text}" );
+					System.Console.ResetColor();
 					break;
 
 				case ConsoleMessageType.Fatal:
-					GD.PrintErr( $"FATAL: {message.Text}" );
+					System.Console.ForegroundColor = ConsoleColor.Red;
+					GD.PrintErr( $"[FATAL] {message.Text}" );
+					System.Console.ResetColor();
 					break;
 			}
 		}
 
 		public void OnUpdate()
-		{
-
-		}
-
-		public void Shutdown()
 		{
 
 		}
