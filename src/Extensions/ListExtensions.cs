d9067806f99d81351c05372e108a6054083e5110
Admer <49600504+Admer456@users.noreply.github.com>
2023-03-11 23:31:20 +0100

    Add ListExtensions for Span conversion

---
+++ src/Extensions/ListExtensions.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Runtime.InteropServices;
+
+namespace Elegy.Extensions
+{
+	public static class ListExtensions
+	{
+		public static Span<T> AsSpan<T>( this List<T> list )
+		{
+			return CollectionsMarshal.AsSpan( list );
+		}
+
+		public static Span<T> AsSpan<T>( this List<T> list, int start )
+		{
+			return list.AsSpan().Slice( start );
+		}
+
+		public static Span<T> AsSpan<T>( this List<T> list, int start, int length )
+		{
+			return list.AsSpan().Slice( start, length );
+		}
+	}
+}
