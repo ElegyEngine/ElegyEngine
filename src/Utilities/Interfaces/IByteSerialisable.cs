46d51301b2888633f4ec9e003afe624814fca05a
Admer <49600504+Admer456@users.noreply.github.com>
2023-03-11 23:33:38 +0100

    Add IByteSerialisable

---
+++ src/Utilities/Interfaces/IByteSerialisable.cs
@@ -0,0 +1,16 @@
+﻿// SPDX-FileCopyrightText: 2022-2023 Admer Šuko
+// SPDX-License-Identifier: MIT
+
+namespace Elegy.Utilities.Interfaces
+{
+	/// <summary>
+	/// This type can be serialised into an array of bytes, and then
+	/// subconsequently used in IByteBuffer.Write/ReadObject.
+	/// </summary>
+	public interface IByteSerialisable
+	{
+		public void Serialise( IByteBuffer buffer );
+	
+		public void Deserialise( IByteBuffer buffer );
+	}
+}
