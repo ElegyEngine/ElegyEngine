f559c39cf518e62162fde64feb2bac6de062fa40
Admer <49600504+Admer456@users.noreply.github.com>
2023-03-12 18:03:32 +0100

    Add console message colours

---
+++ src/Console/Colours.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elegy
+{
+	public partial class Console
+	{
+		public const string Red = "$r";
+		public const string Orange = "$o";
+		public const string Yellow = "$y";
+		public const string Green = "$g";
+		public const string Blue = "$b";
+		public const string Pink = "$p";
+		public const string White = "$w";
+		public const string Gray = "$G";
+	}
+}
