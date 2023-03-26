336060919105ecea3868f279b088549bd243f048
Admer <49600504+Admer456@users.noreply.github.com>
2023-03-27 01:56:13 +0200

    Bring back NodeExtensions

---
+++ src/Extensions/NodeExtensions.cs
@@ -0,0 +1,48 @@
+﻿// SPDX-FileCopyrightText: 2022-2023 Admer Šuko
+// SPDX-License-Identifier: MIT
+
+namespace Elegy.Extensions
+{
+	public static class NodeExtensions
+	{
+		/// <summary>
+		/// Creates a node and attaches it to this node.
+		/// </summary>
+		public static T CreateChild<T>( this Node parent ) where T : Node, new()
+		{
+			T child = new T();
+			parent.AddChild( child );
+			return child;
+		}
+
+		public static Vector3 Forward( this Node3D node )
+		{
+			return node.GlobalTransform.Forward();
+		}
+
+		public static Vector3 Right( this Node3D node )
+		{
+			return node.GlobalTransform.Right();
+		}
+
+		public static Vector3 Up( this Node3D node )
+		{
+			return node.GlobalTransform.Up();
+		}
+
+		public static Vector3 ForwardLocal( this Node3D node )
+		{
+			return node.Transform.Forward();
+		}
+
+		public static Vector3 RightLocal( this Node3D node )
+		{
+			return node.Transform.Right();
+		}
+
+		public static Vector3 UpLocal( this Node3D node )
+		{
+			return node.Transform.Up();
+		}
+	}
+}
