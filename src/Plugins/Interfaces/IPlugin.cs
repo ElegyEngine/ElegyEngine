ce7faaadf05aae7d99a2e66276215e62ba30ff80
Admer <49600504+Admer456@users.noreply.github.com>
2023-05-14 16:58:52 +0200

    IPlugin: comments

--- src/Plugins/Interfaces/IPlugin.cs
+++ src/Plugins/Interfaces/IPlugin.cs
@@ -3,13 +3,34 @@
 
 namespace Elegy
 {
+	/// <summary>
+	/// Generic plugin interface.
+	/// </summary>
 	public interface IPlugin
 	{
+		/// <summary>
+		/// Called when the plugin is being initialised.
+		/// </summary>
 		bool Init();
+
+		/// <summary>
+		/// Called when the engine and plugins are shutting down.
+		/// </summary>
 		void Shutdown();
 
+		/// <summary>
+		/// The name of this plugin.
+		/// </summary>
 		string Name { get; }
+
+		/// <summary>
+		/// If <see cref="Init"/> returned <c>false</c>, the error message.
+		/// </summary>
 		string Error { get; }
+
+		/// <summary>
+		/// Set by <see cref="Init"/> once initialisation succeeds.
+		/// </summary>
 		bool Initialised { get; }
 	}
 }
