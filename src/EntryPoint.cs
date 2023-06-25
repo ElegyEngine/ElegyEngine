e6c09eb304a59d2a8ec06f3a3df4df20763464d0
Admer <49600504+Admer456@users.noreply.github.com>
2023-06-25 22:21:45 +0200

    EntryPoint: documentation comments

--- src/EntryPoint.cs
+++ src/EntryPoint.cs
@@ -3,24 +3,40 @@
 
 namespace Elegy
 {
+	/// <summary>
+	/// Effectively the entry point into the engine. The Godot-based launcher app
+	/// loads this DLL, calls EntryPoint.Init and so on.
+	/// </summary>
 	public static class EntryPoint
 	{
+		/// <summary>
+		/// The entry point, effectively our Main method.
+		/// </summary>
 		public static bool Init( Node3D rootNode )
 		{
 			mEngine = new( rootNode, OS.GetCmdlineUserArgs() );
 			return mEngine.Init();
 		}
 
+		/// <summary>
+		/// Called every frame by Elegy.Launcher.
+		/// </summary>
 		public static void Update( float delta )
 		{
 			mEngine.Update( delta );
 		}
 
+		/// <summary>
+		/// Called every physics frame by Elegy.Launcher.
+		/// </summary>
 		public static void PhysicsUpdate( float delta )
 		{
 			mEngine.PhysicsUpdate( delta );
 		}
 
+		/// <summary>
+		/// Called for every input event by Elegy.Launcher.
+		/// </summary>
 		public static void HandleInput( InputEvent @event )
 		{
 			mEngine.HandleInput( @event );
