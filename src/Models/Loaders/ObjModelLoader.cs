c44f547e61c367780b26a16815bd3751ac246fa2
Admer <49600504+Admer456@users.noreply.github.com>
2023-06-27 15:00:50 +0200

    ObjModelLoader stub

---
+++ src/Models/Loaders/ObjModelLoader.cs
@@ -0,0 +1,30 @@
+﻿// SPDX-FileCopyrightText: 2022-2023 Admer Šuko
+// SPDX-License-Identifier: MIT
+
+using Elegy.Assets;
+
+namespace Elegy.Models.Loaders
+{
+	/// <summary>
+	/// Built-in OBJ loader.
+	/// </summary>
+	public class ObjModelLoader : IModelLoader
+	{
+		/// <inheritdoc/>
+		public bool CanLoad( string path )
+		{
+			if ( !path.EndsWith( ".obj" ) )
+			{
+				return false;
+			}
+
+			return true;
+		}
+
+		/// <inheritdoc/>
+		public Model? LoadModel( string path )
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
