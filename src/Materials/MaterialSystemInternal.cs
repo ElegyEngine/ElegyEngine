c3f458e9153564f9752c10113316eec256d5eed5
Admer <49600504+Admer456@users.noreply.github.com>
2023-08-17 17:43:01 +0200

    Material system: internal stub

---
+++ src/Materials/MaterialSystemInternal.cs
@@ -0,0 +1,25 @@
+﻿// SPDX-FileCopyrightText: 2023 Admer Šuko
+// SPDX-License-Identifier: MIT
+
+namespace Elegy
+{
+	/// <summary>
+	/// Material system implementation.
+	/// </summary>
+	internal sealed class MaterialSystemInternal
+	{
+		private List<Material> mMaterials = new();
+
+		public Material? LoadMaterial( string materialName )
+		{
+			return null;
+		}
+
+		public bool UnloadMaterial( ref Material material )
+		{
+			return false;
+		}
+
+		public IReadOnlyList<Material> GetMaterialList() => mMaterials;
+	}
+}
