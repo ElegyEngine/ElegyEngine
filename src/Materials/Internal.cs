10a9eb6030c3bcd22c269e02ee26755034239e7f
Admer <49600504+Admer456@users.noreply.github.com>
2023-08-17 17:43:18 +0200

    Material system: frontend draft

---
+++ src/Materials/Internal.cs
@@ -0,0 +1,13 @@
+﻿// SPDX-FileCopyrightText: 2023 Admer Šuko
+// SPDX-License-Identifier: MIT
+
+namespace Elegy
+{
+	public static partial class Materials
+	{
+		internal static void SetMaterialSystem( MaterialSystemInternal system )
+			=> mMaterialSystem = system;
+
+		private static MaterialSystemInternal? mMaterialSystem;
+	}
+}
---
+++ src/Materials/Main.cs
@@ -0,0 +1,37 @@
+﻿// SPDX-FileCopyrightText: 2023 Admer Šuko
+// SPDX-License-Identifier: MIT
+
+namespace Elegy
+{
+	/// <summary>
+	/// Material system.
+	/// </summary>
+	public static partial class Materials
+	{
+		/// <summary>
+		/// Loads a material from the material library.
+		/// </summary>
+		/// <param name="materialName">Name of the material.</param>
+		/// <returns>
+		/// A valid <see cref="Material"/> instance on success, <see langword="null"/> on failure.
+		/// </returns>
+		public static Material? LoadMaterial( string materialName )
+			=> mMaterialSystem.LoadMaterial( materialName );
+
+		/// <summary>
+		/// Unloads the <paramref name="material"/>. It is a reference, so it will
+		/// become <see langword="null"/> after this.
+		/// </summary>
+		/// <returns>
+		/// <see langword="true"/> on success, <see langword="false"/>
+		/// if the material is invalid or otherwise.
+		/// </returns>
+		public static bool UnloadMaterial( ref Material material )
+			=> mMaterialSystem.UnloadMaterial( ref material );
+
+		/// <summary>
+		/// All the materials in the system.
+		/// </summary>
+		public static IReadOnlyList<Material> AllMaterials => mMaterialSystem.GetMaterialList();
+	}
+}
