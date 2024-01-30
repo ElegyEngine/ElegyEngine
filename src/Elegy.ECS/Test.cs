4d5800458fa03035041f9fb866ffa5ef98ce830c
Admer <49600504+Admer456@users.noreply.github.com>
2024-01-30 02:11:07 +0100

    Custom ECS implementation experiment

---
+++ src/Elegy.ECS/Test.cs
@@ -0,0 +1,65 @@
+﻿// SPDX-FileCopyrightText: 2024 Admer Šuko
+// SPDX-License-Identifier: MIT
+
+namespace Elegy.ECS;
+
+public struct BreakableComponent : IComponent
+{
+	public int Id { get; set; }
+
+	public BreakableComponent()
+	{
+
+	}
+}
+
+public struct GenericComponent : IComponent
+{
+	public int Id { get; set; }
+
+	public GenericComponent()
+	{
+
+	}
+}
+
+public class BreakableSystem : System<BreakableComponent>
+{
+	public BreakableSystem( World world, int capacity )
+		: base( world, capacity )
+	{
+
+	}
+}
+
+public class Door : Entity
+{
+	public void Spawn()
+	{
+		AddComponent<BreakableComponent>();
+	}
+
+	public void OnOpen( Entity opener )
+	{
+		if ( opener.HasComponent<BreakableComponent>() )
+		{
+			// do stuff
+		}
+	}
+}
+
+public static class EcsTest
+{
+	public static void Test()
+	{
+		World world = new( 1024 );
+		world.RegisterComponent<GenericComponent>( 1000 );
+		world.RegisterSystem<BreakableComponent>( new BreakableSystem( world, 1000 ) );
+
+		Door door = world.CreateEntity<Door>();
+		door.Spawn();
+		door.AddComponent<GenericComponent>();
+		door.AddComponent<BreakableComponent>();
+
+	}
+}
