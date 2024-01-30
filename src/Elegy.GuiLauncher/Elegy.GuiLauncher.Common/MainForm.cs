688fe201313c0ef58dbfdac6442b6ea1681958f3
Admer <49600504+Admer456@users.noreply.github.com>
2024-01-30 18:35:41 +0100

    GuiLauncher: use the new stuff

--- src/Elegy.GuiLauncher/Elegy.GuiLauncher.Common/MainForm.cs
+++ src/Elegy.GuiLauncher/Elegy.GuiLauncher.Common/MainForm.cs
@@ -5,9 +5,94 @@ using System.Runtime.InteropServices;
 
 namespace Elegy.GuiLauncher
 {
+	internal class FormLogger : IConsoleFrontend
+	{
+		private TextArea mText;
+
+		public string Name => "Eto.Forms Logger";
+
+		public string Error { get; set; } = string.Empty;
+
+		public bool Initialised { get; set; }
+
+		public FormLogger( TextArea text )
+		{
+			mText = text;
+		}
+
+		public bool Init()
+		{
+			Initialised = true;
+			return true;
+		}
+
+		public void OnLog( string message, ConsoleMessageType type, float timeSubmitted )
+		{
+			string messageNoColour = string.Empty;
+			for ( int i = 0; i < message.Length; i++ )
+			{
+				if ( message[i] == '$' && i < message.Length - 1 )
+				{
+					i += 2;
+				}
+
+				messageNoColour += message[i];
+			}
+
+			mText.Text += $"{messageNoColour}\n";
+		}
+
+		public void OnUpdate( float delta )
+		{
+
+		}
+
+		public void Shutdown()
+		{
+
+		}
+	}
+
+	internal class FormApp : IApplication
+	{
+		public string Name => "Form App";
+
+		public string Error => string.Empty;
+
+		public bool Initialised { get; private set; }
+
+		public bool Init()
+		{
+			Initialised = true;
+			return true;
+		}
+
+		public int Clicks = 4;
+
+		public bool RunFrame( float delta )
+		{
+			Console.Log( $"Remaining clicks: {Clicks + 1}" );
+			Clicks--;
+
+			return Clicks > 0;
+		}
+
+		public void Shutdown()
+		{
+			Initialised = false;
+		}
+
+		public bool Start()
+		{
+			return true;
+		}
+	}
+
 	public partial class MainForm : Form
 	{
 		private SilkWindow mSilkWindow;
+		private FormLogger mLogger;
+		private TextArea mTextArea;
 		private Engine mEngine;
 
 		private Command CreateButton( string name, EventHandler<EventArgs> action )
@@ -28,55 +113,44 @@ namespace Elegy.GuiLauncher
 			mEngine = engine;
 
 			Title = "My Eto Form";
-			MinimumSize = new Size( 1024, 768 );
+			MinimumSize = new( 1280, 960 );
 
-			mSilkWindow = new SilkWindow()
+			//mSilkWindow = new SilkWindow()
+			//{
+			//	Size = new Size( 800, 600 )
+			//};
+
+			mTextArea = new()
 			{
-				Size = new Size( 800, 600 )
+				Size = new( 1200, 900 ),
+				ReadOnly = true
 			};
+
+			mLogger = new( mTextArea );
 
 			Content = new StackLayout
 			{
 				Padding = 10,
 				Items =
 				{
-					"Hello World!",
-					mSilkWindow
+					"Engine output:",
+					mTextArea
 				}
 			};
 
-			// create a few commands that can be used for the menu and toolbar
-			var clickMe = new Command { MenuText = "Click Me!", ToolBarText = "Click Me!" };
-			clickMe.Executed += ( sender, e ) => MessageBox.Show( this, "I was clicked!" );
-
-			// create menu
-			Menu = new MenuBar
-			{
-				Items =
-				{
-					// File submenu
-					new SubMenuItem { Text = "&File", Items = { clickMe } },
-				},
-				ApplicationItems =
-				{
-					new ButtonMenuItem { Text = "&Preferences..." }
-				},
-				QuitItem = CreateButton( "Quit", (sender, e) => Application.Instance.Quit() ),
-				AboutItem = CreateButton( "About...", (sender, e) => new AboutDialog().ShowDialog( this ) )
-			};
-
-			// create toolbar			
 			ToolBar = new ToolBar
 			{
 				Items =
 				{
 					CreateButton( "Start engine", (sender, e) =>
 					{
-						if ( !mEngine.Init() )
+						if ( !mEngine.Init( mLogger ) )
 						{
 							MessageBox.Show( mEngine.ShutdownReason );
 							return;
 						}
+
+						Plugins.RegisterPlugin( new FormApp() );
 					} ),
 
 					CreateButton( "Run engine", (sender, e) =>
