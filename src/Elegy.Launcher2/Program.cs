0bdcdc4875188adde10be60c849bc30490f033b8
Admer <49600504+Admer456@users.noreply.github.com>
2024-01-30 02:32:01 +0100

    Elegy.Launcher2

---
+++ src/Elegy.Launcher2/Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Elegy.Launcher2
+{
+	internal class Program
+	{
+		static void PrintError( string message )
+		{
+			System.Console.BackgroundColor = ConsoleColor.Red;
+			System.Console.ForegroundColor = ConsoleColor.Black;
+			System.Console.WriteLine( message );
+			System.Console.BackgroundColor = ConsoleColor.Black;
+			System.Console.ForegroundColor = ConsoleColor.White;
+		}
+
+		static void Main( string[] args )
+		{
+			System.Console.Title = "Elegy test game";
+
+			Engine engine = new( args );
+
+			while ( !engine.Init() )
+			{
+				if ( engine.ShutdownReason is null )
+				{
+					PrintError( $"Engine failed to initialise: reason unknown" );
+				}
+				else
+				{
+					PrintError( $"Engine failed to initialise: '{engine.ShutdownReason}'" );
+				}
+
+				System.Console.WriteLine( "Press ESC to exit. Any other key will retry." );
+				if ( System.Console.ReadKey().Key == ConsoleKey.Escape )
+				{
+					return;
+				}
+			}
+
+			engine.Run();
+		}
+	}
+}
