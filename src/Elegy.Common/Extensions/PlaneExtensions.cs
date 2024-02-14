7a6f4b4963a425954d2eb1e197000cbb69dc5f5d
Admer <49600504+Admer456@users.noreply.github.com>
2024-02-14 04:45:27 +0100

    PlaneExtensions: documentation

--- src/Elegy.Common/Extensions/PlaneExtensions.cs
+++ src/Elegy.Common/Extensions/PlaneExtensions.cs
@@ -5,8 +5,15 @@ using Elegy.Maths;
 
 namespace Elegy.Extensions
 {
+	/// <summary>
+	/// Elegy-specific <see cref="Plane"/> extensions. Mainly for TrenchBroom map compilation.
+	/// </summary>
 	public static class PlaneExtensions
 	{
+		/// <summary>
+		/// Gets the closest axis to the plane's normal. This is for <see cref="Assets.BrushMapData.Brush"/> faces
+		/// that do not have paraxial texture coordinates.
+		/// </summary>
 		public static Vector3 GetClosestAxis( this Plane plane )
 		{
 			Vector3 normal = plane.Normal.Abs();
@@ -24,6 +31,10 @@ namespace Elegy.Extensions
 			return Vector3.UnitZ;
 		}
 
+		/// <summary>
+		/// Gets the closest axis to the plane's normal. This is for <see cref="Assets.BrushMapData.Brush"/> faces
+		/// that do not have paraxial texture coordinates.
+		/// </summary>
 		public static Vector3D GetClosestAxis( this PlaneD plane )
 		{
 			Vector3D normal = plane.Normal.Abs();
