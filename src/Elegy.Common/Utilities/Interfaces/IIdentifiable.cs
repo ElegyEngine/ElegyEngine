360685053afd5eb44a48bae0e2fd3249433858a9
Admer <49600504+Admer456@users.noreply.github.com>
2024-02-14 04:44:09 +0100

    Utilities: add IIdentifiable and ITransformable

---
+++ src/Elegy.Common/Utilities/Interfaces/IIdentifiable.cs
@@ -0,0 +1,16 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+namespace Elegy.Utilities.Interfaces
+{
+	/// <summary>
+	/// A thing containing an ID.
+	/// </summary>
+	public interface IIdentifiable
+	{
+		/// <summary>
+		/// ID of the object.
+		/// </summary>
+		int Id { get; init; }
+	}
+}
