5d7a9c33780fbed6a0e138afcd20190fc6d29bbb
Admer <49600504+Admer456@users.noreply.github.com>
2024-02-14 04:45:16 +0100

    ListExtensions: documentation

--- src/Elegy.Common/Extensions/ListExtensions.cs
+++ src/Elegy.Common/Extensions/ListExtensions.cs
@@ -3,18 +3,31 @@ using System.Runtime.InteropServices;
 
 namespace Elegy.Extensions
 {
+	/// <summary>
+	/// Elegy-specific <see cref="List{T}"/> extensions.
+	/// </summary>
 	public static class ListExtensions
 	{
+		/// <summary>
+		/// Sometimes <c>AsSpan</c> is not available for lists, so we can always have this.
+		/// </summary>
 		public static Span<T> AsSpan<T>( this List<T> list )
 		{
 			return CollectionsMarshal.AsSpan( list );
 		}
 
+		/// <summary>
+		/// Interprets a <see cref="List{T}"/> as a <see cref="Span{T}"/> but starting from the <paramref name="start"/>'th element.
+		/// </summary>
 		public static Span<T> AsSpan<T>( this List<T> list, int start )
 		{
 			return list.AsSpan().Slice( start );
 		}
 
+		/// <summary>
+		/// Interprets a <see cref="List{T}"/> as a <see cref="Span{T}"/> but starting from the <paramref name="start"/>'th element,
+		/// going up to <paramref name="length"/> elements after that.
+		/// </summary>
 		public static Span<T> AsSpan<T>( this List<T> list, int start, int length )
 		{
 			return list.AsSpan().Slice( start, length );
