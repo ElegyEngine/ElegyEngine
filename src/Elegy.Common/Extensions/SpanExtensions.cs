73b0c1d56468291f8eb6346a796bb8cf8b04aad2
Admer <49600504+Admer456@users.noreply.github.com>
2024-02-14 04:45:49 +0100

    SpanExtensions: documentation

--- src/Elegy.Common/Extensions/SpanExtensions.cs
+++ src/Elegy.Common/Extensions/SpanExtensions.cs
@@ -3,8 +3,14 @@ using System.Runtime.InteropServices;
 
 namespace Elegy.Extensions
 {
+	/// <summary>
+	/// Elegy-specific <see cref="Span{T}"/> extensions.
+	/// </summary>
 	public static class SpanExtensions
 	{
+		/// <summary>
+		/// A shortcut to <see cref="MemoryMarshal.GetReference{T}(Span{T})"/>.
+		/// </summary>
 		public static ref T GetRef<T>( this Span<T> span )
 		{
 			return ref MemoryMarshal.GetReference( span );
