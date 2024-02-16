0eee9e2ec1f6832995a245e78e74927f28117ac3
Admer <49600504+Admer456@users.noreply.github.com>
2024-02-16 23:36:48 +0100

    Add licence headers to tests

--- src/Elegy.Common.Tests/Assets/MaterialDocumentTests.cs
+++ src/Elegy.Common.Tests/Assets/MaterialDocumentTests.cs
@@ -1,3 +1,5 @@
+// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
 
 using Elegy.Assets;
 
--- src/Elegy.Common.Tests/Usings.cs
+++ src/Elegy.Common.Tests/Usings.cs
@@ -1,2 +1,4 @@
+// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
 
 global using Xunit;
