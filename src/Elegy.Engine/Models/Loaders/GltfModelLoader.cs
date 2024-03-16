00bb0860c8f083b1a5361e306d57fb8cff1f89fb
Admer <49600504+Admer456@users.noreply.github.com>
2024-03-16 19:26:08 +0100

    Add GltfModelLoader stub

---
+++ src/Elegy.Engine/Models/Loaders/GltfModelLoader.cs
@@ -0,0 +1,30 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using Elegy.Assets;
+
+namespace Elegy.Models.Loaders
+{
+	/// <summary>
+	/// Built-in GLTF loader.
+	/// </summary>
+	public class GltfModelLoader : IModelLoader
+	{
+		/// <inheritdoc/>
+		public bool CanLoad( string path )
+		{
+			if ( !path.EndsWith( ".gltf" ) && !path.EndsWith( ".glb" ) )
+			{
+				return false;
+			}
+
+			return true;
+		}
+
+		/// <inheritdoc/>
+		public Model? LoadModel( string path )
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
