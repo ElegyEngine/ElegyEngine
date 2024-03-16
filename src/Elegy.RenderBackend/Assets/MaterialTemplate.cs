d651d0cc8cc7820099451489bf21b1862622e58f
Admer <49600504+Admer456@users.noreply.github.com>
2024-03-16 16:43:33 +0100

    Add material and shader template assets

---
+++ src/Elegy.RenderBackend/Assets/MaterialTemplate.cs
@@ -0,0 +1,38 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using FaceCullMode = Veldrid.FaceCullMode;
+
+namespace Elegy.RenderBackend.Assets
+{
+	public enum Blending
+	{
+		Opaque,
+		Transparent,
+		Additive,
+		Multiply,
+		MiddleGray,
+	}
+
+	public class PipelineInfo
+	{
+		public FaceCullMode FaceCulling { get; set; } = FaceCullMode.Back;
+
+		public Blending BlendMode { get; set; } = Blending.Opaque;
+
+		public bool AlphaTest { get; set; } = false;
+
+		public float AlphaTestThreshold { get; set; } = 0.5f;
+	}
+
+	public class MaterialTemplate
+	{
+		public string Name { get; set; } = string.Empty;
+
+		public string ShaderTemplate { get; set; } = string.Empty;
+
+		public string? Polyshader { get; set; }
+
+		public PipelineInfo PipelineInfo { get; set; } = new();
+	}
+}
---
+++ src/Elegy.RenderBackend/Assets/ShaderTemplate.cs
@@ -0,0 +1,23 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+namespace Elegy.RenderBackend.Assets
+{
+	public class MaterialParameter
+	{
+		public string Name { get; set; } = string.Empty;
+
+		public string ShaderName { get; set; } = string.Empty;
+
+		public ShaderDataType Type { get; set; } = ShaderDataType.Float;
+	}
+
+	public class ShaderTemplate
+	{
+		public string Name { get; set; } = string.Empty;
+
+		public List<MaterialParameter> Parameters { get; set; } = new();
+
+		public List<ShaderTemplateEntry> ShaderVariants { get; set; } = new();
+	}
+}
