2346cb40a72bcf23a0751db872b8fb142637dce9
Admer <49600504+Admer456@users.noreply.github.com>
2024-03-19 21:31:11 +0100

    GltfModelLoader: draft

--- src/Elegy.Engine/Models/Loaders/GltfModelLoader.cs
+++ src/Elegy.Engine/Models/Loaders/GltfModelLoader.cs
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: MIT
 
 using Elegy.Assets;
+using SharpGLTF.Scenes;
+using SharpGLTF.Schema2;
 
 namespace Elegy.ModelLoaders
 {
@@ -10,6 +12,8 @@ namespace Elegy.ModelLoaders
 	/// </summary>
 	public class GltfModelLoader : IModelLoader
 	{
+		private TaggedLogger mLogger = new();
+
 		/// <inheritdoc/>
 		public bool CanLoad( string path )
 		{
@@ -24,7 +28,57 @@ namespace Elegy.ModelLoaders
 		/// <inheritdoc/>
 		public Model? LoadModel( string path )
 		{
-			throw new NotImplementedException();
+			ModelRoot gltfScene;
+			try
+			{
+				gltfScene = SceneBuilder.LoadDefaultScene( path, new()
+				{
+					Validation = SharpGLTF.Validation.ValidationMode.Skip
+				} )
+				.ToGltf2( new()
+				{
+					MergeBuffers = false,
+					UseStridedBuffers = true
+				} );
+			}
+			catch ( Exception ex )
+			{
+				mLogger.Error( $"Couldn't load model {path}, message:\n{ex.Data}" );
+				return null;
+			}
+
+			foreach ( var mesh in gltfScene.LogicalMeshes )
+			{
+				mLogger.Log( $"Mesh '{mesh.Name}'" );
+				foreach ( var primitive in mesh.Primitives )
+				{
+					mLogger.Log( $"  * Primitive {primitive.GetHashCode()}" );
+					mLogger.Log( $"    * Material {primitive.Material.Name}" );
+
+					foreach ( var vertexAccessor in primitive.VertexAccessors )
+					{
+						mLogger.Log( $"    * Vertex input {vertexAccessor.Key}" );
+
+						switch ( vertexAccessor.Key )
+						{	// TODO: fill in the data
+							case "POSITION": break;
+							case "NORMAL": break;
+							case "UV0": break;
+							case "UV1": break;
+							case "COLOR0": break;
+							case "COLOR1": break;
+							case "TANGENT": break;
+							case "BITANGENT": break;
+						}
+					}
+				}
+			}
+
+			return new Model()
+			{
+				Name = path,
+				Meshes = new()
+			};
 		}
 	}
 }
