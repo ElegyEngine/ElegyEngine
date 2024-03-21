7ca8d72682fe15d6ea3c1815dc99a79bf2a0aa16
Admer <49600504+Admer456@users.noreply.github.com>
2024-03-21 23:50:25 +0100

    Texture: FromData and GenerateMissingTexturePattern

--- src/Elegy.Engine/Resources/Texture.cs
+++ src/Elegy.Engine/Resources/Texture.cs
@@ -1,6 +1,7 @@
 ﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
 // SPDX-License-Identifier: MIT
 
+using Elegy.Engine.API;
 using Elegy.Engine.Interfaces.Rendering;
 
 namespace Elegy.Engine.Resources
@@ -9,17 +10,52 @@ namespace Elegy.Engine.Resources
 	public class Texture
 	{
 		/// <summary></summary>
-		public Texture FromData( int width, int height, Span<byte> bytes )
-		{
+		public static Texture FromData( int width, int height, Span<byte> bytes )
+			=> new()
+			{
+				Width = width,
+				Height = height,
+				RenderTexture = Render.Instance.CreateTexture( width, height, 1, bytes )
+			};
 
+		/// <summary></summary>
+		public static byte[] GenerateMissingTexturePattern()
+		{
+			const int Stride = 16 * 4;
+
+			byte[] textureBytes = new byte[16 * 16 * 4];
+			for ( int y = 0; y < 16; y++ )
+			{
+				for ( int x = 0; x < 16; x++ )
+				{
+					int index = y * Stride + x * 4;
+
+					textureBytes[index + 3] = 255;
+
+					if ( (y % 4 == 0) || (x % 4 == 0) )
+					{
+						textureBytes[index] = 240;
+						textureBytes[index + 1] = 240;
+						textureBytes[index + 2] = 240;
+					}
+					else
+					{
+						textureBytes[index] = (byte)(50.0f - 40.0f * MathF.Sin( x / 5.0f ));
+						textureBytes[index] = (byte)(60.0f + 50.0f * MathF.Sin( y / 5.0f ));
+						textureBytes[index] = (byte)(50.0f + 50.0f * MathF.Sin( (x + y) / 5.0f ));
+					}
+				}
+			}
+
+			return textureBytes;
 		}
 
 		/// <summary></summary>
-		public int Width { get; }
+		public int Width { get; init; }
 		/// <summary></summary>
-		public int Height { get; }
+		public int Height { get; init; }
 
 		/// <summary></summary>
-		public ITexture? RenderTexture { get; } = null;
+		public ITexture? RenderTexture { get; init; } = null;
 	}
 }
