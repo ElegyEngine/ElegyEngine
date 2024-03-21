cd2f888d6be8c08f7306f75e8372808b1d95ab3f
Admer <49600504+Admer456@users.noreply.github.com>
2024-03-21 20:21:24 +0100

    Split up AssetSystemInternal into Materials and Models section

---
+++ src/Elegy.Engine/AssetSystemInternal.Models.cs
@@ -0,0 +1,13 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using Elegy.Common.Assets;
+using Elegy.Engine.API;
+
+namespace Elegy.Engine
+{
+	internal partial class AssetSystemInternal
+	{
+
+	}
+}
