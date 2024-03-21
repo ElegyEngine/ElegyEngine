a45689ab97193a78818ab6dd89a6c16226b6a8a9
Admer <49600504+Admer456@users.noreply.github.com>
2024-03-22 00:27:19 +0100

    ITextureLoader: add LoadTexture

--- src/Elegy.Engine/Interfaces/ITextureLoader.cs
+++ src/Elegy.Engine/Interfaces/ITextureLoader.cs
@@ -8,6 +8,15 @@ namespace Elegy.Engine.Interfaces
 	/// </summary>
 	public interface ITextureLoader : IAssetLoader
 	{
-		
+		/// <summary>
+		/// Loads a texture from the given <paramref name="path"/>.
+		/// </summary>
+		/// <param name="path">Full path to an image.</param>
+		/// <param name="width">Output width in pixels.</param>
+		/// <param name="height">Output height in pixels.</param>
+		/// <param name="depth">If it's a 3D texture, how many layers.</param>
+		/// <param name="bytes">The output pixel bytes.</param>
+		/// <returns><see langword="true"/> upon success, <see langword="false"/> otherwise.</returns>
+		bool LoadTexture( string path, out int width, out int height, out int depth, out Span<byte> bytes );
 	}
 }
