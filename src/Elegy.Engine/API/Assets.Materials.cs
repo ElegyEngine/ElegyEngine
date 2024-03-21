dffd1fa6bd598972591b5f57cc379c9da7b067c1
Admer <49600504+Admer456@users.noreply.github.com>
2024-03-22 00:29:30 +0100

    Forgot a using

--- src/Elegy.Engine/API/Assets.Materials.cs
+++ src/Elegy.Engine/API/Assets.Materials.cs
@@ -1,6 +1,7 @@
 ﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
 // SPDX-License-Identifier: MIT
 
+using Elegy.Engine.Interfaces;
 using Elegy.Engine.Resources;
 
 namespace Elegy.Engine.API
