ee583a01c92794f22b25c40fc615c6b7b4275591
Admer <49600504+Admer456@users.noreply.github.com>
2024-03-23 21:33:34 +0100

    Game: add basic client controller

---
+++ src/Elegy.TestGame/Client/BasicController.cs
@@ -0,0 +1,30 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+namespace TestGame.Client
+{
+	public class BasicController : IPlayerControllable
+	{
+		Vector3 mPosition = new();
+		Vector3 mDirection = new();
+		Vector3 mViewAngles = new();
+
+		public void Update( float dt )
+		{
+			mPosition += mDirection * dt;
+		}
+
+		public PlayerControllerState GenerateControllerState()
+			=> new()
+			{
+				Position = mPosition,
+				Angles = mViewAngles
+			};
+
+		public void HandleClientInput( ClientCommands commands )
+		{
+			mViewAngles = commands.ViewAngles;
+			mDirection = commands.MovementDirection;
+		}
+	}
+}
