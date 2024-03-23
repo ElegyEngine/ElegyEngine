ce471e505de2ac0d430be6d8ef398b2622bc205d
Admer <49600504+Admer456@users.noreply.github.com>
2024-03-23 21:34:37 +0100

    TextureMetadata: comments and more solid Is3D logic

--- src/Elegy.Common/Assets/TextureMetadata.cs
+++ src/Elegy.Common/Assets/TextureMetadata.cs
@@ -8,36 +8,49 @@ namespace Elegy.Common.Assets
 	/// </summary>
 	public enum TextureCompression
 	{
+		/// <summary> No compression. </summary>
 		None,
+		/// <summary> DXT1, 1-bit alpha or opaque. </summary>
 		Dxt1,
+		/// <summary> DXT5, alphablend or opaque. </summary>
 		Dxt5,
+		/// <summary> Universal ASTC, can do just about anything. </summary>
 		Uastc,
+		/// <summary> ETC, imagine DXT5 but for mobile. </summary>
 		Etc1s,
 	}
 
-	/// <summary>Represents info about the texture, without its actual data.</summary>
+	/// <summary> Represents info about the texture, without its actual data. </summary>
 	public class TextureMetadata
 	{
 		/// <summary> Width in pixels. </summary>
 		public uint Width { get; init; }
+
 		/// <summary> Height in pixels. </summary>
 		public uint Height { get; init; }
+
 		/// <summary> Depth in pixels. </summary>
 		public uint Depth { get; init; }
+
 		/// <summary> Components per pixel (RGB, grayscale, RGBA...) </summary>
 		public uint Components { get; init; } = 4;
+
 		/// <summary> Number of bytes per pixel. </summary>
 		public uint BytesPerPixel { get; init; } = 1;
+
 		/// <summary> Is this a floating-point texture? </summary>
 		public bool Float { get; init; } = false;
+
 		/// <summary> Type of texture compression, if any. </summary>
 		public TextureCompression Compression { get; init; } = TextureCompression.None;
 
 		/// <summary> Is this a 1D texture? </summary>
 		public bool Is1D => Height == 0 && Depth == 0;
+
 		/// <summary> Is this a 2D texture? </summary>
 		public bool Is2D => Depth == 0;
+		
 		/// <summary> Is this a 3D texture? </summary>
-		public bool Is3D => !Is2D;
+		public bool Is3D => !Is1D && !Is2D;
 	}
 }
