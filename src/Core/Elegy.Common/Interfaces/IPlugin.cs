95be6ed990fc83c7eeb636740631755f8553d543
Admer <49600504+Admer456@users.noreply.github.com>
2024-04-09 04:18:53 +0200

    Common: get core plugin interfaces

--- src/Core/Elegy.Common/Interfaces/IPlugin.cs
+++ src/Core/Elegy.Common/Interfaces/IPlugin.cs
@@ -1,7 +1,7 @@
 ﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
 // SPDX-License-Identifier: MIT
 
-namespace Elegy.Engine.Interfaces
+namespace Elegy.Common.Interfaces
 {
 	/// <summary>
 	/// Generic plugin interface.
