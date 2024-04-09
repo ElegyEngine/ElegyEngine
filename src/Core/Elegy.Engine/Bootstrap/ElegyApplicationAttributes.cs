6d301a8862aba4202189e9d9c856cd742c9d8091
Admer <49600504+Admer456@users.noreply.github.com>
2024-04-09 04:21:05 +0200

    Engine: add app framework attributes

---
+++ src/Core/Elegy.Engine/Bootstrap/ElegyApplicationAttributes.cs
@@ -0,0 +1,45 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+namespace Elegy.Engine.Bootstrap
+{
+	/// <summary> Marks the entry point for Elegy's application framework. </summary>
+	[AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
+	public class ElegyMainAttribute : Attribute { }
+
+	/// <summary> Initialise the asset subsystem. </summary>
+	[AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
+	public class WithAssetSystemAttribute : Attribute { }
+
+	/// <summary> Initialise the audio subsystem. </summary>
+	[AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
+	public class WithAudioSystemAttribute : Attribute { }
+
+	/// <summary> Initialise the console subsystem. </summary>
+	[AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
+	public class WithConsoleSystemAttribute : Attribute { }
+
+	/// <summary> Initialise the virtual file system. </summary>
+	[AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
+	public class WithFileSystemAttribute : Attribute { }
+
+	/// <summary> Initialise the input subsystem. </summary>
+	[AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
+	public class WithInputSystemAttribute : Attribute { }
+
+	/// <summary> Initialise the plugin subsystem. </summary>
+	[AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
+	public class WithPluginSystemAttribute : Attribute { }
+
+	/// <summary> Initialise the rendering subsystem. </summary>
+	[AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
+	public class WithRenderSystemAttribute : Attribute { }
+
+	/// <summary> A shortcut to initialise all needed systems for games. </summary>
+	[AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
+	public class WithAllGameSystemsAttribute : Attribute { }
+
+	/// <summary> A shortcut to initialise all needed systems for basic tools. </summary>
+	[AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
+	public class WithMinimalToolSystemsAttribute : Attribute { }
+}
