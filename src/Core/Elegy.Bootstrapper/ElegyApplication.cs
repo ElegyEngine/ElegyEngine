902d2c7a1c8342fa9e5dff71a31446a12c1ac060
Admer <49600504+Admer456@users.noreply.github.com>
2024-04-09 04:25:09 +0200

    Add Elegy.Bootstrapper

---
+++ src/Core/Elegy.Bootstrapper/ElegyApplication.cs
@@ -0,0 +1,55 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using Elegy.Common.Assets;
+
+namespace Elegy.Bootstrapper
+{
+	public static partial class ElegyApplication
+	{
+		public static void Validate( string region, in LaunchConfig config, Func<LaunchConfig, bool> initialiser )
+		{
+			if ( !initialiser( config ) )
+			{
+				throw new Exception( region );
+			}
+		}
+
+		static partial void Init_Generated( in LaunchConfig config );
+		static partial void Shutdown_Generated();
+		public static void Run( in LaunchConfig config, Action loop )
+		{
+			try
+			{
+				Init_Generated( config );
+			}
+			catch ( Exception ex )
+			{
+				Console.Error.WriteLine( $"ERROR: couldn't initialise {ex.Message}" );
+				Shutdown_Generated();
+				return;
+			}
+
+			loop();
+			Shutdown_Generated();
+		}
+
+		[ElegyMain]
+		[WithAssetSystem]
+		public static void Main( string[] args )
+			=> ElegyApplication.Run( 
+			config: new()
+			{
+				Args = args,
+				Engine = new()
+				{
+					BaseFolder = "game"
+				}
+			},
+
+			loop: () =>
+			{
+				// update the engine, render frames etc.
+			} );
+	}
+}
---
+++ src/Core/Elegy.Bootstrapper/ElegyApplicationAttributes.cs
@@ -0,0 +1,31 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using System;
+
+namespace Elegy.Bootstrapper
+{
+	[AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
+	public class ElegyMainAttribute : Attribute
+	{
+
+	}
+
+	[AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
+	public class WithAssetSystemAttribute : Attribute
+	{
+
+	}
+
+	[AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
+	public class WithConsoleSystemAttribute : Attribute
+	{
+
+	}
+
+	[AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
+	public class WithPluginSystemAttribute : Attribute
+	{
+
+	}
+}
