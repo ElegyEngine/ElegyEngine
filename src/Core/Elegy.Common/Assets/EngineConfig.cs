f47b929637604c0312979a84f925ec680bde2a3c
Admer <49600504+Admer456@users.noreply.github.com>
2024-04-12 01:14:56 +0200

    EngineConfig: better default values

--- src/Core/Elegy.Common/Assets/EngineConfig.cs
+++ src/Core/Elegy.Common/Assets/EngineConfig.cs
@@ -13,12 +13,18 @@ namespace Elegy.Common.Assets
 		}
 
 		/// <summary></summary>
-		public string? ConfigName { get; set; } = null;
+		public string? ConfigName { get; set; } = "Default";
 		/// <summary></summary>
 		public string EngineFolder { get; set; } = "engine";
 		/// <summary></summary>
 		public string BaseFolder { get; set; } = "game";
 		/// <summary></summary>
-		public string[] EnginePlugins { get; set; } = Array.Empty<string>();
+		public string[] EnginePlugins { get; set; } =
+		[
+			"plugins/RenderStandard",
+			// We don't have audio yet, so this one shall wait
+			//"plugins/AudioStandard",
+			"plugins/DevConsole"
+		];
 	}
 }
