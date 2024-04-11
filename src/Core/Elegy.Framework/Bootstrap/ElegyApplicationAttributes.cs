b617eb2390f6f2044b50c26b3c3fca77d7ad09d8
Admer <49600504+Admer456@users.noreply.github.com>
2024-04-12 01:35:06 +0200

    Engine: rename to Framework, move to Modules etc.
    * Remove console commands
    * Update package info

---
+++ src/Core/Elegy.Framework/Bootstrap/ElegyApplicationAttributes.cs
@@ -0,0 +1,41 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+namespace Elegy.Framework.Bootstrap
+{
+	/// <summary> Initialise the asset subsystem. </summary>
+	[AttributeUsage( AttributeTargets.Class, AllowMultiple = false )]
+	public class ElegyBootstrapAttribute : Attribute { }
+
+	/// <summary> Initialise the asset subsystem. </summary>
+	[AttributeUsage( AttributeTargets.Class, AllowMultiple = false )]
+	public class WithAssetSystemAttribute : Attribute { }
+
+/// <summary> Initialise the audio subsystem. </summary>
+[AttributeUsage( AttributeTargets.Class, AllowMultiple = false )]
+	public class WithAudioSystemAttribute : Attribute { }
+
+	/// <summary> Initialise the console subsystem. </summary>
+	[AttributeUsage( AttributeTargets.Class, AllowMultiple = false )]
+	public class WithConsoleSystemAttribute : Attribute { }
+
+	/// <summary> Initialise the virtual file system. </summary>
+	[AttributeUsage( AttributeTargets.Class, AllowMultiple = false )]
+	public class WithFileSystemAttribute : Attribute { }
+
+	/// <summary> Initialise the input subsystem. </summary>
+	[AttributeUsage( AttributeTargets.Class, AllowMultiple = false )]
+	public class WithInputSystemAttribute : Attribute { }
+
+	/// <summary> Initialise the platform subsystem. </summary>
+	[AttributeUsage( AttributeTargets.Class, AllowMultiple = false )]
+	public class WithPlatformSystemAttribute : Attribute { }
+
+	/// <summary> Initialise the plugin subsystem. </summary>
+	[AttributeUsage( AttributeTargets.Class, AllowMultiple = false )]
+	public class WithPluginSystemAttribute : Attribute { }
+
+	/// <summary> Initialise the rendering subsystem. </summary>
+	[AttributeUsage( AttributeTargets.Class, AllowMultiple = false )]
+	public class WithRenderSystemAttribute : Attribute { }
+}
--- src/Core/Elegy.Framework/Globals.cs
+++ src/Core/Elegy.Framework/Globals.cs
@@ -1,8 +1,4 @@
 ﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
 // SPDX-License-Identifier: MIT
 
-global using StringDictionary = System.Collections.Generic.Dictionary<string, string>;
 global using Console = Elegy.ConsoleSystem.API.Console;
-
-global using Elegy.Common.Extensions;
-global using System.Reflection;
