0ed56e533437696594061212d4e19fdc90639c18
Admer <49600504+Admer456@users.noreply.github.com>
2024-04-13 00:24:06 +0200

    RenderBackend: formatting

--- src/Core/Elegy.RenderBackend/Extensions/DeviceExtensions.cs
+++ src/Core/Elegy.RenderBackend/Extensions/DeviceExtensions.cs
@@ -8,18 +8,22 @@ namespace Elegy.RenderBackend.Extensions
 {
 	public static class DeviceExtensions
 	{
-		public static DeviceBuffer CreateBufferFromList<T>(this GraphicsDevice device, BufferUsage usage, List<T> list) where T : unmanaged
+		public static DeviceBuffer CreateBufferFromList<T>( this GraphicsDevice device, BufferUsage usage, List<T> list )
+			where T: unmanaged
 		{
-			ReadOnlySpan<T> span = CollectionsMarshal.AsSpan(list);
-			var buffer = device.ResourceFactory.CreateBufferForList(usage, list);
-			device.UpdateBuffer(buffer, 0, span);
+			ReadOnlySpan<T> span = CollectionsMarshal.AsSpan( list );
+			var buffer = device.ResourceFactory.CreateBufferForList( usage, list );
+			device.UpdateBuffer( buffer, 0, span );
 			return buffer;
 		}
 
-		public static DeviceBuffer CreateBufferFromStruct<T>(this GraphicsDevice device, BufferUsage usage, ref T obj) where T : unmanaged
+		public static DeviceBuffer CreateBufferFromStruct<T>( this GraphicsDevice device, BufferUsage usage, ref T obj )
+			where T: unmanaged
 		{
-			var buffer = device.ResourceFactory.CreateBufferForStruct<T>(usage);
-			device.UpdateBuffer(buffer, 0, ref obj);
+			var buffer = device.ResourceFactory.CreateBufferForStruct<T>( usage );
+			device.UpdateBuffer( buffer, 0, ref obj );
+			return buffer;
+		}
 			return buffer;
 		}
 	}
