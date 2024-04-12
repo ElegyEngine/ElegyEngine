7a3db7da2da9d055863ce8e651e2c072a069fb4f
Admer <49600504+Admer456@users.noreply.github.com>
2024-04-13 00:23:43 +0200

    Mesh: use arrays

--- src/Core/Elegy.Common/Assets/MeshData/Mesh.cs
+++ src/Core/Elegy.Common/Assets/MeshData/Mesh.cs
@@ -21,32 +21,32 @@ namespace Elegy.Common.Assets.MeshData
 		public string MaterialName { get; set; } = string.Empty;
 
 		/// <summary> Vertex positions. </summary>
-		public IList<Vector3> Positions { get; set; } = Array.Empty<Vector3>();
+		public Vector3[] Positions { get; set; } = Array.Empty<Vector3>();
 		/// <summary> Vertex normals. </summary>
-		public IList<Vector3> Normals { get; set; } = Array.Empty<Vector3>();
+		public Vector3[] Normals { get; set; } = Array.Empty<Vector3>();
 		/// <summary> Vertex tangents. </summary>
-		public IList<Vector4> Tangents { get; set; } = Array.Empty<Vector4>();
+		public Vector4[] Tangents { get; set; } = Array.Empty<Vector4>();
 		/// <summary> Vertex texture coordinates, channel 0. </summary>
-		public IList<Vector2> Uv0 { get; set; } = Array.Empty<Vector2>();
+		public Vector2[] Uv0 { get; set; } = Array.Empty<Vector2>();
 		/// <summary> Vertex texture coordinates, channel 1. </summary>
-		public IList<Vector2> Uv1 { get; set; } = Array.Empty<Vector2>();
+		public Vector2[] Uv1 { get; set; } = Array.Empty<Vector2>();
 		/// <summary> Vertex texture coordinates, channel 2. </summary>
-		public IList<Vector2> Uv2 { get; set; } = Array.Empty<Vector2>();
+		public Vector2[] Uv2 { get; set; } = Array.Empty<Vector2>();
 		/// <summary> Vertex texture coordinates, channel 3. </summary>
-		public IList<Vector2> Uv3 { get; set; } = Array.Empty<Vector2>();
+		public Vector2[] Uv3 { get; set; } = Array.Empty<Vector2>();
 		/// <summary> Vertex colours, channel 0. </summary>
-		public IList<Vector4> Color0 { get; set; } = Array.Empty<Vector4>();
+		public Vector4B[] Color0 { get; set; } = Array.Empty<Vector4B>();
 		/// <summary> Vertex colours, channel 1. </summary>
-		public IList<Vector4> Color1 { get; set; } = Array.Empty<Vector4>();
+		public Vector4B[] Color1 { get; set; } = Array.Empty<Vector4B>();
 		/// <summary> Vertex colours, channel 2. </summary>
-		public IList<Vector4> Color2 { get; set; } = Array.Empty<Vector4>();
+		public Vector4B[] Color2 { get; set; } = Array.Empty<Vector4B>();
 		/// <summary> Vertex colours, channel 3. </summary>
-		public IList<Vector4> Color3 { get; set; } = Array.Empty<Vector4>();
+		public Vector4B[] Color3 { get; set; } = Array.Empty<Vector4B>();
 		/// <summary> Vertex bone indices. </summary>
-		public IList<Vector4B> BoneIndices { get; set; } = Array.Empty<Vector4B>();
+		public Vector4B[] BoneIndices { get; set; } = Array.Empty<Vector4B>();
 		/// <summary> Normalised vertex bone weights. </summary>
-		public IList<Vector4> BoneWeights { get; set; } = Array.Empty<Vector4>();
+		public Vector4[] BoneWeights { get; set; } = Array.Empty<Vector4>();
 		/// <summary> Vertex indices. </summary>
-		public IList<uint> Indices { get; set; } = Array.Empty<uint>();
+		public uint[] Indices { get; set; } = Array.Empty<uint>();
 	}
 }
