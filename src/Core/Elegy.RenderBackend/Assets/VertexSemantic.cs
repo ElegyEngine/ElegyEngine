8c28d9132de4785e67253fb129e035506c15923c
Admer <49600504+Admer456@users.noreply.github.com>
2024-04-13 00:21:07 +0200

    RenderBackend: add VertexSemantic
    * It is richer than Veldrid's VertexElementSemantic

---
+++ src/Core/Elegy.RenderBackend/Assets/VertexSemantic.cs
@@ -0,0 +1,26 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+namespace Elegy.RenderBackend.Assets
+{
+	/// <summary>
+	/// The meaning behind a vertex layout element.
+	/// </summary>
+	public enum VertexSemantic
+	{
+		/// <summary> 3D vertex positions. </summary>
+		Position,
+		/// <summary> Vertex normals. </summary>
+		Normal,
+		/// <summary> Vertex tangents. </summary>
+		Tangent,
+		/// <summary> Vertex UVs. </summary>
+		Uv,
+		/// <summary> Vertex colours. </summary>
+		Colour,
+		/// <summary> Vertex bone weights. </summary>
+		BoneWeight,
+		/// <summary> Vertex bone indices. </summary>
+		BoneIndex
+	}
+}
