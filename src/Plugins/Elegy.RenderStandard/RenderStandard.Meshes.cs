0cded325449eb76e0e29937cb79015313dac0124
Admer <49600504+Admer456@users.noreply.github.com>
2024-04-13 00:25:38 +0200

    RenderStandard: RenderMesh draft

--- src/Plugins/Elegy.RenderStandard/RenderStandard.Meshes.cs
+++ src/Plugins/Elegy.RenderStandard/RenderStandard.Meshes.cs
@@ -4,20 +4,74 @@
 using Elegy.Common.Assets;
 using Elegy.AssetSystem.Interfaces;
 using Elegy.AssetSystem.Interfaces.Rendering;
+using Elegy.RenderStandard.Extensions;
 using Elegy.RenderSystem.Interfaces;
 using Elegy.RenderSystem.Interfaces.Rendering;
 
+using Collections.Pooled;
+using Veldrid;
+
+using ModelMesh = Elegy.Common.Assets.MeshData.Mesh;
+
 namespace Elegy.RenderStandard;
+
+public class RenderSubmesh
+{
+	public RenderSubmesh( RenderMaterial material, DeviceBuffer buffer )
+	{
+
+	}
+
+	public RenderMaterial Material;
+	public DeviceBuffer MeshBuffer;
+}
+
+public class RenderMesh : IMesh
+{
+	private GraphicsDevice mDevice;
+	private List<RenderSubmesh> mSubmeshes = new();
+
+	public RenderMesh( GraphicsDevice device, Model modelData )
+	{
+		mDevice = device;
+		Data = modelData;
+
+		foreach ( var modelMesh in modelData.Meshes )
+		{
+			
+		}
+	}
+
+	public Model Data { get; set; }
+
+	public void RegenerateBuffers()
+	{
+		Clear();
+
+
+	}
+
+	public void Clear()
+	{
+		foreach ( var submesh in mSubmeshes )
+		{
+			submesh.MeshBuffer.Dispose();
+		}
+
+		mSubmeshes.Clear();
+	}
+}
 
 public partial class RenderStandard : IRenderFrontend
 {
+	private PooledSet<RenderMesh> mMeshSet = new( 1024 );
+
 	public IMesh CreateMesh( Model modelData )
-	{
-		throw new NotImplementedException();
-	}
+		=> mMeshSet.AddAndGet( new RenderMesh( mDevice, modelData ) );
 
 	public bool FreeMesh( IMesh mesh )
-	{
-		throw new NotImplementedException();
-	}
+		=> mMeshSet.RemoveAndThen( (RenderMesh)mesh, ( mesh ) =>
+		{
+			mesh.Clear();
+		} );
 }
