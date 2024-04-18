112d09c733b64f21c6f890806f42d6f5b0f76f50
Admer <49600504+Admer456@users.noreply.github.com>
2024-04-18 13:38:05 +0200

    MaterialTemplate: make more complete

--- src/Core/Elegy.RenderBackend/Templating/MaterialTemplate.cs
+++ src/Core/Elegy.RenderBackend/Templating/MaterialTemplate.cs
@@ -1,21 +1,42 @@
 ﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
 // SPDX-License-Identifier: MIT
 
+using Elegy.RenderBackend.Assets;
 using Elegy.RenderBackend.Extensions;
 using Veldrid;
 
 namespace Elegy.RenderBackend.Templating
 {
+	public struct VariantVertexAttribute
+	{
+		public VertexSemantic Semantic;
+		public int Channel;
+	}
+
+	public struct VariantResourceMapping
+	{
+		public int LayoutId;
+		public int SetId;
+	}
+
 	public class ShaderVariant
 	{
-		public ShaderVariant( MaterialTemplate parent, ResourceLayout[] layouts, Shader vertexShader, Shader pixelShader, Pipeline pipeline )
+		public ShaderVariant( MaterialTemplate parent, ResourceLayout[] layouts,
+			Shader vertexShader, Shader pixelShader, Pipeline pipeline,
+			VariantVertexAttribute[] attributes, VariantResourceMapping[] mappings )
 		{
 			Template = parent;
 			Layouts = layouts;
 			VertexShader = vertexShader;
 			PixelShader = pixelShader;
 			Pipeline = pipeline;
+			VertexAttributes = attributes;
+			ResourceMappings = mappings;
 		}
+
+		public VariantResourceMapping[] ResourceMappings { get; }
+
+		public VariantVertexAttribute[] VertexAttributes { get; }
 
 		public MaterialTemplate Template { get; }
 
@@ -42,7 +63,16 @@ namespace Elegy.RenderBackend.Templating
 
 		public Dictionary<string, ShaderVariant> ShaderVariants { get; } = new();
 
-		public bool CompileResources( GraphicsDevice gd, Framebuffer outputFramebuffer, Func<string, string?>? pathTo = null )
+		public bool HasVariant( string name )
+			=> ShaderVariants.ContainsKey( name );
+
+		public Pipeline GetVariantPipeline( string name )
+			=> ShaderVariants[name].Pipeline;
+
+		public bool CompileResources( GraphicsDevice gd,
+			Func<ShaderTemplateEntry, bool, OutputDescription> outputDescriptionFunc,
+			int[] skippedSetIds,
+			Func<string, string?>? pathTo = null )
 		{
 			ResourceFactory factory = gd.ResourceFactory;
 
@@ -55,7 +85,43 @@ namespace Elegy.RenderBackend.Templating
 					layouts[i] = factory.CreateLayout( variant.ResourceLayouts[i] );
 				}
 
-				// 2. Create shader objects
+				// 2. Create vertex attribute metadata for linking
+				VariantVertexAttribute[] attributes = new VariantVertexAttribute[variant.VertexLayouts.Count];
+				int numUvChannels = 0;
+				int numColourChannels = 0;
+				for ( int i = 0; i < attributes.Length; i++ )
+				{
+					var semantic = Utils.GetVertexSemantic( variant.VertexLayouts[i].Name );
+					attributes[i] = new()
+					{
+						Semantic = semantic,
+						Channel = semantic switch
+						{
+							VertexSemantic.Uv => numUvChannels++,
+							VertexSemantic.Colour => numColourChannels++,
+							_ => 0
+						}
+					};
+				}
+
+				// 3. Create resource mapping table
+				List<VariantResourceMapping> mappings = new( variant.ResourceLayouts.Count );
+				for ( int i = 0; i < variant.ResourceLayouts.Count; i++ )
+				{
+					var layout = variant.ResourceLayouts[i];
+					if ( skippedSetIds.Contains( layout.Set ) )
+					{
+						continue;
+					}
+
+					mappings.Add( new()
+					{
+						LayoutId = i,
+						SetId = layout.Set
+					} );
+				}
+
+				// 4. Create shader objects
 				string? shaderBasePath = Utils.PathToShaderVariant( ShaderTemplate, variant );
 				if ( pathTo is not null )
 				{
@@ -68,10 +134,11 @@ namespace Elegy.RenderBackend.Templating
 				Shader vertexShader = factory.LoadShaderDirect( shaderBasePath, ShaderStages.Vertex );
 				Shader pixelShader = factory.LoadShaderDirect( shaderBasePath, ShaderStages.Fragment );
 
-				// 3. Create pipelines
-				Pipeline pipeline = factory.CreatePipeline( Data, variant, vertexShader, pixelShader, layouts, outputFramebuffer );
+				// 5. Create pipelines
+				Pipeline pipeline = factory.CreatePipeline( Data, variant, vertexShader, pixelShader, layouts,
+					outputDescriptionFunc( variant, ShaderTemplate.PostprocessHint ) );
 
-				ShaderVariants.Add( variant.ShaderDefine, new( this, layouts, vertexShader, pixelShader, pipeline ) );
+				ShaderVariants.Add( variant.ShaderDefine, new( this, layouts, vertexShader, pixelShader, pipeline, attributes, mappings.ToArray() ) );
 			}
 
 			return true;
