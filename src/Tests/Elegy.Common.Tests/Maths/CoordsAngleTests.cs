a63a966c3f82a43175451f31547676a57c9d4f52
Admer <49600504+Admer456@users.noreply.github.com>
2024-05-08 20:17:39 +0200

    Add coordinate system tests

---
+++ src/Tests/Elegy.Common.Tests/Maths/CoordsAngleTests.cs
@@ -0,0 +1,124 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using Elegy.Common.Extensions;
+using Elegy.Common.Maths;
+using System.Numerics;
+using Xunit.Sdk;
+
+namespace Elegy.Common.Tests.Maths;
+
+public static class CoordsAngleTests
+{
+	internal static void RoughlyEqual( Vector3 value, Vector3 expected )
+	{
+		// This should be enough of a delta to catch most discrepancies
+		if ( (expected - value).Length() > 0.001f )
+		{
+			throw new EqualException( expected, value );
+		}
+	}
+
+	internal static void SimpleAnglesTest( Vector3 angles, Vector3 expectedForward, Vector3? expectedUp = null, Vector3? expectedRight = null )
+	{
+		Coords.DirectionsFromDegrees( angles, out var forward, out var up );
+		Vector3 right = Vector3.Cross( forward, up );
+
+		RoughlyEqual( forward, expectedForward );
+
+		if ( expectedUp is not null )
+		{
+			RoughlyEqual( up, expectedUp.Value );
+		}
+
+		if ( expectedRight is not null )
+		{
+			RoughlyEqual( right, expectedRight.Value );
+		}
+	}
+
+	[Fact]
+	public static void AnglesIdentity()
+		=> SimpleAnglesTest(
+			angles: Vector3.Zero,
+			expectedForward: Coords.Forward,
+			expectedUp: Coords.Up,
+			expectedRight: Coords.Right );
+
+	#region Individual axes
+	[Fact]
+	public static void AnglesPitch90()
+		=> SimpleAnglesTest(
+			angles: new( 90.0f, 0.0f, 0.0f ),
+			expectedForward: Coords.Up,
+			expectedUp: Coords.Back,
+			expectedRight: Coords.Right );
+
+	[Fact]
+	public static void AnglesPitch90Negative()
+		=> SimpleAnglesTest(
+			angles: new( -90.0f, 0.0f, 0.0f ),
+			expectedForward: Coords.Down,
+			expectedUp: Coords.Forward,
+			expectedRight: Coords.Right );
+
+	[Fact]
+	public static void AnglesYaw90()
+		=> SimpleAnglesTest(
+			angles: new( 0.0f, 90.0f, 0.0f ),
+			expectedForward: Coords.Right,
+			expectedUp: Coords.Up,
+			expectedRight: Coords.Back );
+
+	[Fact]
+	public static void AnglesYaw90Negative()
+		=> SimpleAnglesTest(
+			angles: new( 0.0f, -90.0f, 0.0f ),
+			expectedForward: Coords.Left,
+			expectedUp: Coords.Up,
+			expectedRight: Coords.Forward );
+
+	[Fact]
+	public static void AnglesRoll90()
+		=> SimpleAnglesTest(
+			angles: new( 0.0f, 0.0f, 90.0f ),
+			expectedForward: Coords.Forward,
+			expectedUp: Coords.Right,
+			expectedRight: Coords.Down );
+
+	[Fact]
+	public static void AnglesRoll90Negative()
+		=> SimpleAnglesTest(
+			angles: new( 0.0f, 0.0f, -90.0f ),
+			expectedForward: Coords.Forward,
+			expectedUp: Coords.Left,
+			expectedRight: Coords.Up );
+
+	#endregion
+
+	#region Dual axes
+	[Fact]
+	public static void AnglesPitch90Yaw90()
+		=> SimpleAnglesTest(
+			angles: new( 90.0f, 90.0f, 0.0f ),
+			expectedForward: Coords.Up,
+			expectedUp: Coords.Left,
+			expectedRight: Coords.Back );
+
+	[Fact]
+	public static void AnglesPitch90Roll90()
+		=> SimpleAnglesTest(
+			angles: new( 90.0f, 0.0f, 90.0f ),
+			expectedForward: Coords.Up,
+			expectedUp: Coords.Right,
+			expectedRight: Coords.Forward );
+
+	[Fact]
+	public static void AnglesYaw90Roll90()
+		=> SimpleAnglesTest(
+			angles: new( 0.0f, 90.0f, 90.0f ),
+			expectedForward: Coords.Right,
+			expectedUp: Coords.Back,
+			expectedRight: Coords.Down );
+	#endregion
+}
---
+++ src/Tests/Elegy.Common.Tests/Maths/CoordsMatrixTests.cs
@@ -0,0 +1,43 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using Elegy.Common.Extensions;
+using Elegy.Common.Maths;
+using System.Numerics;
+using Xunit.Sdk;
+
+namespace Elegy.Common.Tests.Maths;
+
+public static class CoordsMatrixTests
+{
+	//[Fact]
+	//public static void ViewMatrixIdentity()
+	//{
+	//	var viewMatrix = Coords.CreateViewMatrix( Vector3.Zero, Coords.Forward, Coords.Up );
+	//	Assert.Equal( Matrix4x4.Identity, viewMatrix );
+	//}
+
+	[Fact]
+	public static void ViewMatrixIdentityTransform()
+	{
+		var viewMatrix = Coords.CreateViewMatrix( Vector3.Zero, Coords.Forward, Coords.Up );
+		Vector3 transformedForwardVector = Vector3.Transform( new( 1.0f, 2.0f, 3.0f ), viewMatrix );
+		CoordsAngleTests.RoughlyEqual( new( 1.0f, 3.0f, -2.0f ), transformedForwardVector );
+	}
+
+	[Fact]
+	public static void ViewMatrixIdentityTransform_Yaw90()
+	{
+		Coords.DirectionsFromRadians( Coords.TurnRight / 4.0f, out var forward, out var up );
+	
+		var viewMatrix = Coords.CreateViewMatrix( Vector3.Zero, forward, up );
+
+		// If we turn right, then the point - relative to our view - goes left
+		Vector3 transformedForwardVector = Vector3.Transform( Coords.Forward, viewMatrix );
+		CoordsAngleTests.RoughlyEqual( Coords.Left, transformedForwardVector );
+
+		// If we turn right, then the point - relative to our view - goes left
+		transformedForwardVector = Vector3.Transform( new( 1.0f, 2.0f, 3.0f ), viewMatrix );
+		CoordsAngleTests.RoughlyEqual( new( -2.0f, 3.0f, -1.0f ), transformedForwardVector );
+	}
+}
