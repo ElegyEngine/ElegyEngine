ce1351a1d9ba7f7c89cc9de0fc5d096726a013fd
Admer <49600504+Admer456@users.noreply.github.com>
2024-05-08 20:12:46 +0200

    Add material template JSON parsing options

---
+++ src/Core/Elegy.RenderBackend/Text/MaterialTemplateJsonOptions.cs
@@ -0,0 +1,22 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using Elegy.Common.Text;
+using Elegy.RenderBackend.Assets;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Elegy.RenderBackend.Text
+{
+	public static class MaterialTemplateJsonOptions
+	{
+		public readonly static JsonSerializerOptions Instance = JsonHelpers.Create( new()
+		{
+			Converters =
+			{
+				new JsonStringEnumConverter<Blending>(),
+				new JsonStringEnumConverter<Veldrid.FaceCullMode>()
+			}
+		} );
+	}
+}
