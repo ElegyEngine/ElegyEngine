82883fd6ea96bfdd62119e6dd1521aa80d2c2730
Admer <49600504+Admer456@users.noreply.github.com>
2024-05-25 03:18:55 +0200

    Common: add list extensions from RenderStandard

--- src/Core/Elegy.Common/Extensions/ListExtensions.cs
+++ src/Core/Elegy.Common/Extensions/ListExtensions.cs
@@ -34,5 +34,62 @@ namespace Elegy.Common.Extensions
 		{
 			return list.AsSpan().Slice( start, length );
 		}
+
+		public static T AddAndGet<T>( this ISet<T> set, T element )
+		{
+			set.Add( element );
+			return element;
+		}
+
+		public static T AddAndGet<T>( this IList<T> list, T element )
+		{
+			list.Add( element );
+			return list[list.Count - 1];
+		}
+
+		public static T? TryGetAt<T>( this IList<T> list, int elementIndex )
+		{
+			if ( elementIndex < 0 || elementIndex > list.Count )
+			{
+				return default( T );
+			}
+
+			return list[elementIndex];
+		}
+
+		public static bool RemoveAndThen<T>( this ISet<T> set, T element, Action<T> onRemoved )
+		{
+			if ( set.Remove( element ) )
+			{
+				onRemoved( element );
+				return true;
+			}
+
+			return false;
+		}
+
+		public static bool TryRemove<T>( this IList<T> list, int elementIndex, Action<T> onRemove )
+		{
+			T? element = list.TryGetAt( elementIndex );
+			if ( element is null )
+			{
+				return false;
+			}
+
+			onRemove( element );
+			return true;
+		}
+
+		public static bool TryRemove<T>( this IList<T> list, T element, Action<T> onRemove )
+		{
+			int index = list.IndexOf( element );
+			if ( index == -1 )
+			{
+				return false;
+			}
+
+			onRemove( element );
+			return true;
+		}
 	}
 }
