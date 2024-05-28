e358ed858fa64beba6fa9b7319d9024885240e5c
Admer <49600504+Admer456@users.noreply.github.com>
2024-05-28 02:07:54 +0200

    Port material & texture implementations from R.S.

---
+++ src/Modules/Elegy.RenderSystem/Resources/MaterialParameterPool.cs
@@ -0,0 +1,299 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using Elegy.AssetSystem.Interfaces.Rendering;
+using Elegy.Common.Assets;
+using Elegy.Common.Extensions;
+using Elegy.ConsoleSystem;
+using Elegy.RenderBackend.Extensions;
+using Elegy.RenderBackend.Templating;
+using Elegy.RenderSystem.API;
+
+using System.Numerics;
+using Veldrid;
+
+using ShaderDataType = Elegy.RenderBackend.Assets.ShaderDataType;
+using MaterialParameterLevel = Elegy.RenderBackend.Assets.MaterialParameterLevel;
+using Utils = Elegy.RenderSystem.Resources.MaterialParameterUtils;
+
+// TODO: regenerate resource sets when changing textures and samplers
+// That will allow for toggling between pixelated thingies, anisotropic settings,
+// and reloading textures while developing. It's gonna be a potentially costly operation,
+// so none of that should be used during actual gameplay
+
+namespace Elegy.RenderSystem.Resources
+{
+	public struct ResourceSetVariant
+	{
+		public ShaderVariant ShaderVariant { get; init; }
+		public ResourceSet[] ResourceSets { get; init; }
+	}
+
+	public class MaterialParameterPool
+	{
+		private static TaggedLogger mLogger = new( "MaterialParams" );
+
+		/// <summary>
+		/// Constructor for global material parameters.
+		/// </summary>
+		public MaterialParameterPool( GraphicsDevice device, MaterialTemplate template )
+		{
+			mDevice = device;
+			Template = template;
+
+			ParameterLevel = MaterialParameterLevel.Global;
+			// I arbitrarily chose 4 here, it's not common that there'll be more than that per material template
+			Parameters.EnsureCapacity( 4 );
+
+			foreach ( var parametre in template.ShaderTemplate.Parameters.AsSpan() )
+			{
+				if ( parametre.Level != MaterialParameterLevel.Global )
+				{
+					continue;
+				}
+
+				if ( parametre.Type == ShaderDataType.Buffer || parametre.Type == ShaderDataType.BufferRW )
+				{
+					mLogger.Warning( $"Global parametre '{parametre.Name}' is an unknown buffer type (global mat. param.: {parametre.Name})" );
+					continue;
+				}
+
+				MaterialParameter? globalParam = Render.GetGlobalMaterialParameter( parametre.Name );
+				if ( globalParam is null )
+				{
+					mLogger.Error( $"Global parametre '{parametre.Name}' cannot be found" );
+					continue;
+				}
+
+				Parameters.Add( globalParam );
+			}
+
+			RegenerateParameterResourceSets();
+		}
+
+		/// <summary>
+		/// Constructor for per-instance and data-driven material parameters.
+		/// </summary>
+		public MaterialParameterPool( GraphicsDevice device, MaterialTemplate materialTemplate, MaterialDefinition definition, bool perInstance = false )
+		{
+			mDevice = device;
+			Definition = definition;
+			Template = materialTemplate;
+
+			ParameterLevel = perInstance ? MaterialParameterLevel.Data : MaterialParameterLevel.Instance;
+			Parameters.EnsureCapacity( materialTemplate.ShaderTemplate.Parameters.Count );
+
+			// From the shader parametres, we will look up the given material def's parametres
+			// And then generate buffers for em
+			foreach ( var parametre in materialTemplate.ShaderTemplate.Parameters.AsSpan() )
+			{
+				// Builtin and global parameters are filled in externally
+				if ( parametre.Level != ParameterLevel )
+				{
+					continue;
+				}
+
+				if ( parametre.Type == ShaderDataType.Buffer || parametre.Type == ShaderDataType.BufferRW )
+				{
+					mLogger.Warning( $"Parametre '{parametre.Name}' is an unknown buffer type (mat: {definition.Name})" );
+					continue;
+				}
+
+				string? value = definition.Parameters.GetValueOrDefault( parametre.Name );
+
+				Parameters.Add( parametre.Type.IsTexture() switch
+				{
+					true => new( parametre.Name, parametre.Type, Utils.CreateTextureForMaterialParameter( device, parametre, value ) ),
+					false => (parametre.Type == ShaderDataType.Sampler) switch
+					{
+						true => new( parametre.Name, Utils.GetSamplerByName( value ) ),
+						false => new( parametre.Name, parametre.Type, Utils.CreateBufferForMaterialParameter( device, parametre, value ) )
+					}
+				} );
+			}
+
+			RegenerateParameterResourceSets();
+		}
+
+		private GraphicsDevice mDevice;
+		public MaterialParameterLevel ParameterLevel { get; private set; }
+		public List<ResourceSetVariant> ResourceSetVariants { get; private set; } = new();
+		public List<MaterialParameter> Parameters { get; private set; } = new();
+		public MaterialDefinition? Definition { get; private set; } = null;
+		public MaterialTemplate Template { get; private set; }
+
+		public int GetVariantIndex( string name )
+		{
+			for ( int i = 0; i < ResourceSetVariants.Count; i++ )
+			{
+				if ( ResourceSetVariants[i].ShaderVariant.Data.ShaderDefine == name )
+				{
+					return i;
+				}
+			}
+
+			return -1;
+		}
+
+		public string[] GetParameterNames()
+		{
+			return Parameters.Select( p => p.Name ).ToArray();
+		}
+
+		public int GetParameterIndex( string name )
+		{
+			for ( int i = 0; i < Parameters.Count; i++ )
+			{
+				if ( Parameters[i].Name == name )
+				{
+					return i;
+				}
+			}
+
+			return -1;
+		}
+
+		public void SetParameter( int id, int value )
+		{
+			if ( ValidateIntention( id, ShaderDataType.Int, ShaderDataType.Short, ShaderDataType.Byte ) )
+			{
+				Parameters[id].SetValue( mDevice, value );
+			}
+		}
+
+		public void SetParameter( int id, float value )
+		{
+			if ( ValidateIntention( id, ShaderDataType.Float ) )
+			{
+				Parameters[id].SetValue( mDevice, value );
+			}
+		}
+
+		public void SetParameter( int id, bool value )
+		{
+			if ( ValidateIntention( id, ShaderDataType.Byte ) )
+			{
+				Parameters[id].SetValue( mDevice, value );
+			}
+		}
+
+		public void SetParameter( int id, Vector2 value )
+		{
+			if ( ValidateIntention( id, ShaderDataType.Vec2, ShaderDataType.Vec2Byte ) )
+			{
+				Parameters[id].SetValue( mDevice, value );
+			}
+		}
+
+		public void SetParameter( int id, Vector3 value )
+		{
+			if ( ValidateIntention( id, ShaderDataType.Vec3, ShaderDataType.Vec3Byte ) )
+			{
+				Parameters[id].SetValue( mDevice, value );
+			}
+		}
+
+		public void SetParameter( int id, Vector4 value )
+		{
+			if ( ValidateIntention( id, ShaderDataType.Vec4, ShaderDataType.Vec4Byte ) )
+			{
+				Parameters[id].SetValue( mDevice, value );
+			}
+		}
+
+		public void SetParameter( int id, Matrix4x4 value )
+		{
+			if ( ValidateIntention( id, ShaderDataType.Mat44 ) )
+			{
+				Parameters[id].SetValue( mDevice, value );
+			}
+		}
+
+		public void SetBufferParameter<T>( int id, T bufferValue ) where T : unmanaged
+		{
+			if ( ValidateIntention( id, ShaderDataType.Buffer, ShaderDataType.BufferRW ) )
+			{
+				Parameters[id].SetBufferValue( mDevice, bufferValue );
+			}
+		}
+
+		public void SetTexture( int id, ITexture value )
+		{
+			if ( TextureTypeCompatible( id, value.Width, value.Height, value.Depth ) )
+			{
+				Parameters[id].Texture = ((RenderTexture)value).DeviceTexture;
+				RegenerateParameterResourceSets();
+			}
+		}
+
+		public void SetSampler( int id, Sampler sampler )
+		{
+			if ( Parameters[id].Type == ShaderDataType.Sampler )
+			{
+				Parameters[id].Sampler = sampler;
+				RegenerateParameterResourceSets();
+			}
+		}
+
+		public void RegenerateParameterResourceSets()
+		{
+			ResourceSetVariants.EnsureCapacity( Template.ShaderVariants.Count );
+
+			// Generate resource sets from material-level
+			// parametres and shader variants
+			foreach ( var variantAsset in Template.ShaderTemplate.ShaderVariants )
+			{
+				var variant = Template.ShaderVariants[variantAsset.ShaderDefine];
+				ResourceSetVariants.Add( new()
+				{
+					ShaderVariant = variant,
+					ResourceSets = Utils.GenerateResourceSetsForVariant( mDevice, variant, variantAsset, ParameterLevel, Parameters )
+				} );
+			}
+		}
+
+		private bool TextureTypeCompatible( int id, int width, int height, int depth )
+		{
+			if ( !ValidateIntention( id, ShaderDataType.Texture1D, ShaderDataType.Texture2D, ShaderDataType.Texture3D ) )
+			{
+				return false;
+			}
+
+			if ( Parameters[id].Texture is null )
+			{
+				return false;
+			}
+
+			var texture = Parameters[id].Texture;
+			return Parameters[id].Texture.Type switch
+			{
+				TextureType.Texture1D => height == 0 && depth == 0,
+				TextureType.Texture2D => height > 0 && depth == 0,
+				TextureType.Texture3D => height > 0 && depth > 0,
+				_ => false
+			};
+		}
+
+		private bool ValidateIntention( int id, params ShaderDataType[] types )
+		{
+			if ( id >= Parameters.Count )
+			{
+				mLogger.Error( $"Index is out of range ({id})" );
+				return false;
+			}
+
+			if ( !types.Contains( Parameters[id].Type ) )
+			{
+				mLogger.Error( $"Type mismatch, parameter is {Parameters[id].Type} but value is {types[0]} ({Parameters[id].Name})" );
+				return false;
+			}
+
+			return true;
+		}
+
+		public void Dispose()
+		{
+
+		}
+	}
+}
---
+++ src/Modules/Elegy.RenderSystem/Resources/MaterialParameterUtils.cs
@@ -0,0 +1,169 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using Elegy.AssetSystem.API;
+using Elegy.Common.Extensions;
+using Elegy.Common.Maths;
+using Elegy.Common.Utilities;
+using Elegy.RenderBackend.Assets;
+using Elegy.RenderBackend.Extensions;
+using Elegy.RenderBackend.Templating;
+using Elegy.RenderSystem.API;
+using System.Diagnostics;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using Veldrid;
+
+namespace Elegy.RenderSystem.Resources
+{
+	public static class MaterialParameterUtils
+	{
+		public static ResourceSet[] GenerateResourceSetsForVariant( GraphicsDevice device, ShaderVariant variant,
+			RenderBackend.Assets.ShaderTemplateEntry item, MaterialParameterLevel level, List<MaterialParameter> parameters )
+		{
+			// This loop generates a ResourceSet
+			int setId = 0;
+			List<ResourceSetDescription> resourceSetDescriptions = new( item.ResourceLayouts.Count );
+			for ( int i = 0; i < item.ResourceLayouts.Count; i++ )
+			{
+				if ( item.ResourceLayouts[i].Level != level )
+				{
+					continue;
+				}
+
+				// This loop generates the elements for a ResourceSet
+				BindableResource[] bindableResources = new BindableResource[item.ResourceLayouts[i].Elements.Count];
+				ResourceLayout layout = variant.Layouts[i];
+				for ( int e = 0; e < item.ResourceLayouts[i].Elements.Count; e++ )
+				{
+					var elements = item.ResourceLayouts[i].Elements;
+					var element = elements[e];
+					int index = elements.FindIndex( p => p.Name == element.Name );
+					Debug.Assert( index >= 0 );
+
+					if ( element.Type.IsTexture() )
+					{
+						bindableResources[e] = parameters[index].Texture;
+					}
+					else if ( element.Type == ShaderDataType.Sampler )
+					{
+						bindableResources[e] = parameters[index].Sampler;
+					}
+					else
+					{
+						bindableResources[e] = parameters[index].Buffer;
+					}
+				}
+
+				resourceSetDescriptions.Add( new()
+				{
+					Layout = variant.Layouts[i],
+					BoundResources = bindableResources
+				} );
+			}
+
+			return resourceSetDescriptions
+				.Select( rsd => device.ResourceFactory.CreateResourceSet( rsd ) )
+				.ToArray();
+		}
+
+		public static DeviceBuffer CreateBufferForMaterialParameter( GraphicsDevice device, RenderBackend.Assets.MaterialParameter parameter, string? value )
+		{
+			if ( value is null )
+			{
+				return GetDefaultMaterialParameterBuffer( device, parameter.Type );
+			}
+
+			return parameter.Type switch
+			{
+				ShaderDataType.Byte => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, (byte)Parse.Int( value ) ),
+				ShaderDataType.Short => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, (short)Parse.Int( value ) ),
+				ShaderDataType.Int => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, Parse.Int( value ) ),
+				ShaderDataType.Float => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, Parse.Float( value ) ),
+				ShaderDataType.Vec2 => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, value.ToVector2() ),
+				ShaderDataType.Vec3 => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, value.ToVector3() ),
+				ShaderDataType.Vec4 => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, value.ToVector4() ),
+				// Byte variants are written as 0-255 in the file, so when we parse them, convert to 0-1 so they can be converted to 0-255 again internally.
+				// Wew. Could've just written some parsing code for these fellas, but oh well!
+				ShaderDataType.Vec2Byte => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, (Vector2Byte)(value.ToVector2() / 255.0f) ),
+				ShaderDataType.Vec3Byte => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, (Vector3Byte)(value.ToVector3() / 255.0f) ),
+				ShaderDataType.Vec4Byte => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, new Vector4B( value.ToVector4() / 255.0f ) ),
+				_ => throw new NotSupportedException()
+			};
+		}
+
+		public static DeviceBuffer GetDefaultMaterialParameterBuffer( GraphicsDevice device, ShaderDataType type )
+			=> type switch
+			{
+				ShaderDataType.Byte => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, (byte)0 ),
+				ShaderDataType.Short => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, (short)0 ),
+				ShaderDataType.Int => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, 0 ),
+				ShaderDataType.Float => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, 0.0f ),
+				ShaderDataType.Vec2 => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, Vector2.Zero ),
+				ShaderDataType.Vec3 => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, Vector3.Zero ),
+				ShaderDataType.Vec4 => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, Vector4.Zero ),
+				ShaderDataType.Vec2Byte => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, new Vector2Byte( 0, 0 ) ),
+				ShaderDataType.Vec3Byte => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, new Vector3Byte( 0, 0, 0 ) ),
+				ShaderDataType.Vec4Byte => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, Vector4B.Zero ),
+				ShaderDataType.Mat44 => device.CreateBufferFromStruct( BufferUsage.UniformBuffer, Matrix4x4.Identity ),
+				_ => throw new NotSupportedException()
+			};
+
+		public static Sampler GetSamplerByName( string? value )
+		{
+			if ( value is null )
+			{
+				return Render.Samplers.Linear;
+			}
+
+			return value.ToLower() switch
+			{
+				"nearest" => Render.Samplers.Nearest,
+				"linear" => Render.Samplers.Linear,
+				"aniso2" => Render.Samplers.Aniso2x,
+				"aniso4" => Render.Samplers.Aniso4x,
+				"aniso8" => Render.Samplers.Aniso8x,
+				"aniso16" => Render.Samplers.Aniso16x,
+
+				"nearestclamp" => Render.Samplers.NearestClamp,
+				"linearclamp" => Render.Samplers.LinearClamp,
+				"aniso2clamp" => Render.Samplers.Aniso2xClamp,
+				"aniso4clamp" => Render.Samplers.Aniso4xClamp,
+				"aniso8clamp" => Render.Samplers.Aniso8xClamp,
+				"aniso16clamp" => Render.Samplers.Aniso16xClamp,
+
+				"nearestborder" => Render.Samplers.NearestBorder,
+				"linearborder" => Render.Samplers.LinearBorder,
+				"aniso2border" => Render.Samplers.Aniso2xBorder,
+				"aniso4border" => Render.Samplers.Aniso4xBorder,
+				"aniso8border" => Render.Samplers.Aniso8xBorder,
+				"aniso16border" => Render.Samplers.Aniso16xBorder,
+
+				_ => Render.Samplers.Linear
+			};
+		}
+
+		public static Texture CreateTextureForMaterialParameter( GraphicsDevice device, RenderBackend.Assets.MaterialParameter parameter, string? value )
+		{
+			if ( value is null )
+			{
+				return GetMissingTexture();
+			}
+
+			// TODO: SRGB hinting?
+			var texture = Assets.LoadTexture( value, false );
+			if ( texture is null )
+			{
+				return GetMissingTexture();
+			}
+
+			Debug.Assert( texture.RenderTexture is not null );
+			return ((RenderTexture)texture.RenderTexture).DeviceTexture;
+		}
+
+		public static Texture GetMissingTexture()
+		{
+			return ((RenderTexture)Assets.MissingTexture.RenderTexture).DeviceTexture;
+		}
+	}
+}
