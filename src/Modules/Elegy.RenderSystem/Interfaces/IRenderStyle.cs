f2ac4ec629547ebcfbe1698bb6008682f0293a57
Admer <49600504+Admer456@users.noreply.github.com>
2024-06-03 18:23:28 +0200

    RenderStyleDefault: RenderEntities -> RenderSurfaces

--- src/Modules/Elegy.RenderSystem/Interfaces/IRenderStyle.cs
+++ src/Modules/Elegy.RenderSystem/Interfaces/IRenderStyle.cs
@@ -2,12 +2,27 @@
 // SPDX-License-Identifier: MIT
 
 using Elegy.Common.Interfaces;
+using Elegy.RenderBackend;
 using Elegy.RenderSystem.Objects;
-
+using Elegy.RenderSystem.Resources;
 using Veldrid;
 
 namespace Elegy.RenderSystem.Interfaces
 {
+	/// <summary>
+	/// A render surface, nice for rendering in batches.
+	/// It represents one draw call essentially: a mesh to render, together with shader parametres.
+	/// </summary>
+	public struct RenderSurface
+	{
+		/// <summary> The render mesh. </summary>
+		public ArrayMesh Mesh;
+		/// <summary> The parameter pool. </summary>
+		public MaterialParameterPool ParameterPool;
+		/// <summary> The per-entity resource set. </summary>
+		public ResourceSet PerEntitySet;
+	}
+
 	/// <summary>
 	/// Renderer frontend plugin. Implements rendering techniques for a particular graphical style.
 	/// </summary>
@@ -23,13 +38,13 @@ namespace Elegy.RenderSystem.Interfaces
 		bool CreateCorePipelines();
 
 		/// <summary>Draw one or multiple batches, illuminated by one or more lights.</summary>
-		void RenderBatches( CommandList renderCommand, View view, ReadOnlySpan<Batch> billboards, ReadOnlySpan<Light> lights );
+		void RenderBatches( CommandList renderCommand, View view, ReadOnlySpan<Batch> batches, ReadOnlySpan<Light> lights );
 
 		/// <summary>Draw one or multiple billboards, illuminated by one or more lights.</summary>
 		void RenderBillboards( CommandList renderCommand, View view, ReadOnlySpan<Billboard> billboards, ReadOnlySpan<Light> lights );
 
-		/// <summary>Draw one or multiple mesh entities, illuminated by one or more lights.</summary>
-		void RenderMeshEntities( CommandList renderCommand, View view, ReadOnlySpan<MeshEntity> entities, ReadOnlySpan<Light> lights );
+		/// <summary>Draw one or multiple surfaces, illuminated by one or more lights.</summary>
+		void RenderSurfaces( CommandList renderCommand, View view, ReadOnlySpan<RenderSurface> surfaces, RenderMaterial material, ReadOnlySpan<Light> lights );
 
 		/// <summary>Draw a single volume, illuminated by one or more lights.</summary>
 		void RenderVolume( CommandList renderCommand, View view, Volume volume, ReadOnlySpan<Light> lights );
--- src/Modules/Elegy.RenderSystem/RenderStyleDefault.cs
+++ src/Modules/Elegy.RenderSystem/RenderStyleDefault.cs
@@ -2,13 +2,10 @@
 // SPDX-License-Identifier: MIT
 
 using Elegy.ConsoleSystem;
-using Elegy.RenderBackend.Templating;
 using Elegy.RenderSystem.API;
 using Elegy.RenderSystem.Interfaces;
 using Elegy.RenderSystem.Objects;
 using Elegy.RenderSystem.Resources;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
 using Veldrid;
 
 namespace Elegy.RenderSystem
@@ -54,51 +51,37 @@ namespace Elegy.RenderSystem
 
 		}
 
-		private static void RenderSingleEntity( CommandList commands, MeshEntity entity, View view )
+		public void RenderSurfaces( CommandList renderCommand, View view,
+			ReadOnlySpan<RenderSurface> surfaces, RenderMaterial material, ReadOnlySpan<Light> lights )
 		{
-			for ( int i = 0; i < entity.Mesh.Submeshes.Count; i++ )
-			{
-				// TODO: sort render ents by material
-				var submesh = entity.Mesh.Submeshes[i];
-				var submaterial = entity.Mesh.Materials[i];
-				int variantIndex = submaterial.ParameterPool.GetVariantIndex( "GENERAL" );
-				var shaderVariant = submaterial.Template.ShaderVariants.ElementAt( variantIndex ).Value;
+			int variantIndex = material.ParameterPool.GetVariantIndex( "GENERAL" );
+			var shaderVariant = material.Template.ShaderVariants.ElementAt( variantIndex ).Value;
+			
+			renderCommand.SetPipeline( shaderVariant.Pipeline );
+			renderCommand.SetGraphicsResourceSet( 0, view.PerViewSet );
 
-				commands.SetPipeline( shaderVariant.Pipeline );
+			for ( int i = 0; i < surfaces.Length; i++ )
+			{
+				var surface = surfaces[i];
 
 				// We have a few hardcoded resource set IDs
 				// 0 is always per-frame/per-view data (all about the camera basically)
 				// 1 is always per-entity data (entity transform matrix, bone matrices etc.)
-				commands.SetGraphicsResourceSet( 0, view.PerViewSet );
-				commands.SetGraphicsResourceSet( 1, entity.PerEntitySet );
+				renderCommand.SetGraphicsResourceSet( 1, surface.PerEntitySet );
 
 				// Set shader parametres used by this shader variant
-				var parameterPool = entity.PerInstanceParameterPools[i];
-				Render.SetMaterialResourceSets( commands, submaterial, variantIndex, parameterPool );
+				Render.SetMaterialResourceSets( renderCommand, material, variantIndex, surface.ParameterPool );
 
 				// Send vertex buffers used by this shader variant
-				ReadOnlySpan<VariantVertexAttribute> vertexAttributes = shaderVariant.VertexAttributes;
-				for ( int vertexAttributeId = 0; vertexAttributeId < vertexAttributes.Length; vertexAttributeId++ )
+				foreach ( var vertexAttribute in shaderVariant.VertexAttributes.AsSpan() )
 				{
-					var vertexAttribute = vertexAttributes[vertexAttributeId];
-					var buffer = submesh.GetBuffer( vertexAttribute.Semantic, vertexAttribute.Channel );
-
-					Debug.Assert( buffer is not null, $"The '{vertexAttribute.Semantic}' buffer is MISSING" );
-
-					commands.SetVertexBuffer( (uint)vertexAttributeId, buffer );
+					var buffer = surface.Mesh.GetBuffer( vertexAttribute.Semantic, vertexAttribute.Channel );
+					renderCommand.SetVertexBuffer( vertexAttribute.Id, buffer );
 				}
 
 				// AT LAST, render the damn thing
-				commands.SetIndexBuffer( submesh.IndexBuffer, IndexFormat.UInt32 );
-				commands.DrawIndexed( submesh.NumIndices );
-			}
-		}
-
-		public void RenderMeshEntities( CommandList renderCommand, View view, ReadOnlySpan<MeshEntity> entities, ReadOnlySpan<Light> lights )
-		{
-			foreach ( var entity in entities )
-			{
-				RenderSingleEntity( renderCommand, entity, view );
+				renderCommand.SetIndexBuffer( surface.Mesh.IndexBuffer, IndexFormat.UInt32 );
+				renderCommand.DrawIndexed( surface.Mesh.NumIndices );
 			}
 		}
 
