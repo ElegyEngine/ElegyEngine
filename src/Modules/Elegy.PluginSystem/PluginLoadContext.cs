b1d1976b184e32e0f41dd76f1589fb06e4bab9a4
Admer <49600504+Admer456@users.noreply.github.com>
2024-06-06 01:46:21 +0200

    PluginLoadContext: basic plugin dependency support

--- src/Modules/Elegy.PluginSystem/PluginLoadContext.cs
+++ src/Modules/Elegy.PluginSystem/PluginLoadContext.cs
@@ -10,8 +10,9 @@ namespace Elegy.PluginSystem
 {
 	internal class PluginLoadContext : AssemblyLoadContext
 	{
-		private List<(string, Assembly)> mDependencies = new(); 
 		private TaggedLogger mLogger = new( "DllLoader" );
+		private List<(string, Assembly)> mDependencies = new();
+		private Dictionary<string, List<string>> mSearchPaths = new();
 
 		public PluginLoadContext()
 			: base( isCollectible: true )
@@ -46,6 +47,51 @@ namespace Elegy.PluginSystem
 			return true;
 		}
 
+		public void AddPluginSearchPaths( string plugin, string path )
+		{
+			var paths = mSearchPaths.GetOrAdd( plugin, [] );
+			if ( paths.Contains( path ) )
+			{
+				mLogger.Warning( $"Plugin '{plugin}' already has '{path}' in its search paths" );
+				return;
+			}
+
+			paths.Add( path );
+		}
+
+		public bool ClearPluginSearchPaths( string plugin )
+		{
+			if ( !mSearchPaths.ContainsKey( plugin ) )
+			{
+				mLogger.Warning( $"Plugin '{plugin}' is not in search paths" );
+				return false;
+			}
+
+			mSearchPaths[plugin].Clear();
+			mSearchPaths.Remove( plugin );
+
+			return true;
+		}
+
+		private Assembly? TryLoadingAssemblyInDirectory( string path, string assemblyName )
+		{
+			var dllFiles = Directory.EnumerateFiles( path, "*.dll", SearchOption.AllDirectories );
+			foreach ( var file in dllFiles )
+			{
+				var fileNoExtension = Path.ChangeExtension( file, null );
+				if ( fileNoExtension.EndsWith( assemblyName ) )
+				{
+					// Will this cause infinite recursion? I fear it might call Load
+					Assembly assembly = LoadFromAssemblyPath( file );
+					RegisterDependency( fileNoExtension, assembly );
+
+					return assembly;
+				}
+			}
+
+			return null;
+		}
+
 		protected override Assembly? Load( AssemblyName assemblyName )
 		{
 			mLogger.Verbose( $"'{assemblyName.Name}.dll'" );
@@ -56,6 +102,23 @@ namespace Elegy.PluginSystem
 				if ( dependency.Item1 == assemblyName.Name )
 				{
 					return dependency.Item2;
+				}
+			}
+
+			// Check shared plugin dependencies
+			// TODO: make this rigorous, heavy testing required
+			// This would be best done in a special Elegy test app using the
+			// barebones console app setup (like MapCompiler) with some plugins
+			// that have messed-up dependencies for testing
+			foreach ( var plugin in mSearchPaths )
+			{
+				foreach ( var searchPath in plugin.Value )
+				{
+					Assembly? assembly = TryLoadingAssemblyInDirectory( searchPath, assemblyName.Name );
+					if ( assembly is not null )
+					{
+						return assembly;
+					}
 				}
 			}
 
