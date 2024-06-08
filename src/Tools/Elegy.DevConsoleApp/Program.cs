60a40a6489a8bb27ff051e1bf157aafb1e25d31b
Admer <49600504+Admer456@users.noreply.github.com>
2024-06-08 15:06:17 +0200

    Add DevConsoleApp

---
+++ src/Tools/Elegy.DevConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+namespace Elegy.DevConsoleApp
+{
+	public static class Program
+	{
+		public static void Main( string[] args )
+		{
+
+		}
+	}
+}
