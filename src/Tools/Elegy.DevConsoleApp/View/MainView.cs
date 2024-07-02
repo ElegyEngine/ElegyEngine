80843957b6a974ca85cfa5e6f2df97a1b1643a08
Admer <49600504+Admer456@users.noreply.github.com>
2024-07-03 01:54:22 +0200

    DevConsoleApp: add !quit alias

--- src/Tools/Elegy.DevConsoleApp/View/MainView.cs
+++ src/Tools/Elegy.DevConsoleApp/View/MainView.cs
@@ -66,7 +66,7 @@ namespace Elegy.DevConsoleApp.View
 		{
 			string command = mState.InputText.Trim();
 
-			if ( command == "!exit" )
+			if ( command == "!exit" || command == "!quit" )
 			{
 				mState.IsRunning = false;
 			}
