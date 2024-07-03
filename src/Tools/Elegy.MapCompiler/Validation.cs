74934513c9f7758d95e80d2dd175b3291ab82622
Admer <49600504+Admer456@users.noreply.github.com>
2024-07-03 22:00:36 +0200

    MapCompiler: add geometry validation code

---
+++ src/Tools/Elegy.MapCompiler/Validation.cs
@@ -0,0 +1,28 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using System.Diagnostics;
+
+namespace Elegy.MapCompiler
+{
+	internal class GeoValidation
+	{
+		public static bool Scalar( float x, string message )
+		{
+			bool isNan = x == float.NaN;
+			bool isInfinite = float.IsInfinity( x );
+
+			Debug.Assert( !isNan, $"{message} - got value 'NaN'" );
+			Debug.Assert( !isInfinite, $"{message} - got value 'infinity'" );
+
+			return !isNan && !isInfinite;
+		}
+
+		public static bool Vec3( Vector3 v, string message )
+		{
+			return Scalar( v.X, $"{message} - X" )
+				&& Scalar( v.Y, $"{message} - Y" )
+				&& Scalar( v.Z, $"{message} - Z" );
+		}
+	}
+}
