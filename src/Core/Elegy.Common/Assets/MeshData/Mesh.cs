009f945d1f0703b46f87b17851279414af22d0e7
Admer <49600504+Admer456@users.noreply.github.com>
2024-07-18 03:13:36 +0200

    Mesh: add vertex flag

--- src/Core/Elegy.Common/Assets/MeshData/Mesh.cs
+++ src/Core/Elegy.Common/Assets/MeshData/Mesh.cs
@@ -6,6 +6,29 @@ using Elegy.Common.Maths;
 namespace Elegy.Common.Assets.MeshData
 {
 	/// <summary>
+	/// Flags which describe the vertex layout of a mesh.
+	/// </summary>
+	[Flags]
+	public enum MeshVertexFlags
+	{
+		Positions = 1,
+		Positions2D = 2,
+		Normals = 4,
+		Normals2D = 8,
+		Tangents = 16,
+		Uv0 = 32,
+		Uv1 = 64,
+		Uv2 = 128,
+		Uv3 = 256,
+		Color0 = 512,
+		Color1 = 1024,
+		Color2 = 2048,
+		Color3 = 4096,
+		BoneIndices = 8192,
+		BoneWeights = 16384
+	}
+
+	/// <summary>
 	/// Represents a particular part of a model with a material assigned to it.
 	/// </summary>
 	public class Mesh
@@ -52,6 +75,38 @@ namespace Elegy.Common.Assets.MeshData
 		public Vector4[] BoneWeights { get; set; } = Array.Empty<Vector4>();
 		/// <summary> Vertex indices. </summary>
 		public uint[] Indices { get; set; } = Array.Empty<uint>();
+
+		/// <summary> Gets the vertex layout of the mesh, in form of flags. </summary>
+		public MeshVertexFlags GetVertexFlags()
+		{
+			MeshVertexFlags result = 0;
+
+			void Check<T>( T[] array, MeshVertexFlags flag )
+			{
+				if ( array.Length != 0 )
+				{
+					result |= flag;
+				}
+			}
+
+			Check( Positions, MeshVertexFlags.Positions );
+			Check( Positions2D, MeshVertexFlags.Positions2D );
+			Check( Normals, MeshVertexFlags.Normals );
+			Check( Normals2D, MeshVertexFlags.Normals2D );
+			Check( Tangents, MeshVertexFlags.Tangents );
+			Check( Uv0, MeshVertexFlags.Uv0 );
+			Check( Uv1, MeshVertexFlags.Uv1 );
+			Check( Uv2, MeshVertexFlags.Uv2 );
+			Check( Uv3, MeshVertexFlags.Uv3 );
+			Check( Color0, MeshVertexFlags.Color0 );
+			Check( Color1, MeshVertexFlags.Color1 );
+			Check( Color2, MeshVertexFlags.Color2 );
+			Check( Color3, MeshVertexFlags.Color3 );
+			Check( BoneIndices, MeshVertexFlags.BoneIndices );
+			Check( BoneWeights, MeshVertexFlags.BoneWeights );
+
+			return result;
+		}
 
 		/// <summary> Transforms a sequence of vertex data into another format, e.g. float3 into byte4. </summary>
 		public static TDestination[] Transform<TSource, TDestination>( IList<TSource> source, Func<TSource, TDestination> transform )
