c7d854cdaf178a597b0d92eb2c88e4dac4f962cc
Admer <49600504+Admer456@users.noreply.github.com>
2024-07-18 03:17:33 +0200

    Add ILevelWriter interface

---
+++ src/Modules/Elegy.AssetSystem/Interfaces/ILevelWriter.cs
@@ -0,0 +1,18 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using Elegy.Common.Assets;
+
+namespace Elegy.AssetSystem.Interfaces
+{
+	/// <summary>
+	/// Level writer interface.
+	/// </summary>
+	public interface ILevelWriter : IAssetIo
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		bool WriteLevel( string path, ElegyMapDocument map );
+	}
+}
