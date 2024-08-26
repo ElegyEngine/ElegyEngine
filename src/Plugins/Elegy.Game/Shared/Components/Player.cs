23225c22416f09d08a4f36cdbeb888cd1be26cf8
Admer <49600504+Admer456@users.noreply.github.com>
2024-08-26 21:00:39 +0200

    GameSDK: add Player and Transform stubs

---
+++ src/Plugins/Elegy.Game/Shared/Components/Player.cs
@@ -0,0 +1,17 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using Elegy.ECS;
+
+namespace Game.Shared.Components
+{
+	[GameComponent]
+	public struct Player
+	{
+		[EntityEvent<Entity.SpawnEvent>]
+		public void Spawn()
+		{
+
+		}
+	}
+}
---
+++ src/Plugins/Elegy.Game/Shared/Components/Transform.cs
@@ -0,0 +1,14 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using Elegy.ECS;
+
+namespace Game.Shared.Components
+{
+	[GameComponent]
+	public struct Transform
+	{
+		public Vector3 Position { get; set; }
+		public Quaternion Orientation { get; set; }
+	}
+}
