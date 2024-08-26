ec791119093024843b4e03223c4395d261494797
Admer <49600504+Admer456@users.noreply.github.com>
2024-08-26 20:54:11 +0200

    GameSDK: Entity draft

--- src/Plugins/Elegy.Game/Shared/Entity.cs
+++ src/Plugins/Elegy.Game/Shared/Entity.cs
@@ -1,10 +1,39 @@
 ﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
 // SPDX-License-Identifier: MIT
 
+using Game.Server;
+using EcsEntity = fennecs.Entity;
+
 namespace Game.Shared
 {
-	public class Entity
+	public struct Entity
 	{
+		public delegate void SpawnEvent();
+		public delegate void PostSpawnEvent();
+		public delegate void PrecacheEvent();
 
+		public int Id { get; }
+		public EntityWorld World { get; }
+		public EcsEntity EcsObject { get; }
+
+		public Entity( EntityWorld world, int id )
+		{
+			Id = id;
+			World = world;
+
+			EcsObject = world.EcsWorld.Spawn();
+			EcsObject.Add( this );
+		}
+
+		public void LoadFromKeyvalues( Dictionary<string, string> keys )
+		{
+			
+		}
+
+		public void DispatchEvents<T>()
+			where T: Delegate
+		{
+
+		}
 	}
 }
