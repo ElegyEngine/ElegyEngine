8871ab843aad72cdf4f0e961c4a3bee9d86a362b
Admer <49600504+Admer456@users.noreply.github.com>
2024-08-27 05:13:58 +0200

    GameSDK: proof of concept work on Player

--- src/Plugins/Elegy.Game/Shared/Components/Player.cs
+++ src/Plugins/Elegy.Game/Shared/Components/Player.cs
@@ -2,14 +2,36 @@
 // SPDX-License-Identifier: MIT
 
 using Elegy.ECS;
+using Game.Client;
+using Game.Server;
 
 namespace Game.Shared.Components
 {
 	[GameComponent]
+	[Requires<Transform>]
 	public partial struct Player
 	{
+		public Player() { }
+
+		public IPlayerControllable Controller { get; set; } = new BasicController();
+
 		[EntityEvent<Entity.SpawnEvent>]
 		public void Spawn()
+		{
+			
+		}
+
+		// This event attrib doesn't do anything at the moment,
+		// ServerUpdate and ClientUpdate are called manually elsewhere
+		[SystemEvent<Entity.ServerUpdateEvent>]
+		public static void ServerUpdate( EntityWorld world, float delta )
+		{
+			// Update all player comps on the server
+			//world.Query( ... );
+		}
+
+		[EntityEvent<Entity.ClientUpdateEvent>]
+		public void ClientUpdate( GameClient client, float delta )
 		{
 
 		}
