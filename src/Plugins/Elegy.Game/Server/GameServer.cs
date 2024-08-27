e0ec758263ef576193fffe625420f5c03ee36193
Admer <49600504+Admer456@users.noreply.github.com>
2024-08-27 05:13:18 +0200

    GameSDK: add various methods to GameServer

--- src/Plugins/Elegy.Game/Server/GameServer.cs
+++ src/Plugins/Elegy.Game/Server/GameServer.cs
@@ -1,18 +1,48 @@
 ﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
 // SPDX-License-Identifier: MIT
 
+using Elegy.Common.Assets;
+
 namespace Game.Server
 {
-	public class GameServer
+	public partial class GameServer
 	{
+		public EntityWorld EntityWorld { get; } = new();
+		public int MaxPlayers => Connections.Capacity;
+
+		/// <summary>
+		/// How often (per second) to send game state packets to clients.
+		/// </summary>
+		public int GameSnapshotRate => 10;
+
 		public bool Init()
 		{
+			if ( !EntityWorld.Init() )
+			{
+				return false;
+			}
+
 			return true;
 		}
 
 		public void Shutdown()
 		{
+			EntityWorld.Shutdown();
+		}
 
+		public void Update( float delta )
+		{
+			// TODO: listen to client connection packets
+
+			EntityWorld.Update( delta );
+		}
+
+		public bool Setup( int maxPlayers, ElegyMapDocument level )
+		{
+			Connections.Clear();
+
+			// TODO: later we could also obtain the level name etc.
+			return EntityWorld.Setup( level );
 		}
 	}
 }
