e14371ad66683fef555f35276f806361e5e04f9a
Admer <49600504+Admer456@users.noreply.github.com>
2024-08-27 05:10:12 +0200

    Add Requires and GenerateComponentRegistry attributes

---
+++ src/Core/Elegy.ECS/RequiresAttribute.cs
@@ -0,0 +1,17 @@
+﻿// SPDX-FileCopyrightText: 2024-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+namespace Elegy.ECS
+{
+	public abstract class BaseRequiresAttribute : Attribute
+	{
+		public abstract Type ComponentType { get; }
+	}
+
+	[AttributeUsage( AttributeTargets.Struct, AllowMultiple = true )]
+	public class RequiresAttribute<T> : BaseRequiresAttribute
+		where T : struct
+	{
+		public override Type ComponentType => typeof( T );
+	}
+}
