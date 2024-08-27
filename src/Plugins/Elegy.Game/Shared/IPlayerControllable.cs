88645cb4ef9e00964f4d8b80553e10f6a5ccc3cf
Admer <49600504+Admer456@users.noreply.github.com>
2024-08-27 05:14:21 +0200

    GameSDK: XML docs for IPlayerControllable

--- src/Plugins/Elegy.Game/Shared/IPlayerControllable.cs
+++ src/Plugins/Elegy.Game/Shared/IPlayerControllable.cs
@@ -3,13 +3,20 @@
 
 namespace Game.Shared
 {
-	// What the server sends back to the client
+	/// <summary>
+	/// Movement data the server sends back to the client.
+	/// </summary>
 	public struct PlayerControllerState
 	{
 		public Vector3 Position;
 		public Vector3 Angles; // mmm not actually needed but oh well
 	}
 
+	/// <summary>
+	/// An entity that is controllable by the player. This is not meant
+	/// to be implemented by components! Rather, components can refer
+	/// to a controller object.
+	/// </summary>
 	public interface IPlayerControllable
 	{
 		void Update( float delta );
