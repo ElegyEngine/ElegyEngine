f9c98bb3e68f58556ff29aecefd088606ed17a21
Admer <49600504+Admer456@users.noreply.github.com>
2024-08-27 05:12:11 +0200

    GameSDK: add mouse grabbing to InputSystem

--- src/Plugins/Elegy.Game/Client/InputSystem.cs
+++ src/Plugins/Elegy.Game/Client/InputSystem.cs
@@ -1,18 +1,33 @@
 ﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
 // SPDX-License-Identifier: MIT
 
+using Elegy.InputSystem.API;
+using Silk.NET.Input;
+
 namespace Game.Client
 {
 	public class InputSystem
 	{
 		public bool Init()
 		{
+			// TODO: load keybinds and stuff
+
 			return true;
 		}
 
 		public void Shutdown()
 		{
 
+		}
+
+		public void GrabMouse()
+		{
+			Input.Mouse.Cursor.CursorMode = CursorMode.Hidden;
+		}
+
+		public void ReleaseMouse()
+		{
+			Input.Mouse.Cursor.CursorMode = CursorMode.Normal;
 		}
 	}
 }
