3f675abc50e7d59ac02d2cbff9e2bdd5b9c315da
Admer <49600504+Admer456@users.noreply.github.com>
2024-09-18 04:00:04 +0200

    ECS: add IEntityProperty for custom KV parsing

---
+++ src/Core/Elegy.ECS/IEntityProperty.cs
@@ -0,0 +1,13 @@
+﻿// SPDX-FileCopyrightText: 2024-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+namespace Elegy.ECS
+{
+	public interface IEntityProperty
+	{
+		/// <summary>
+		/// Modifies itself with the parsed data.
+		/// </summary>
+		void ParseEntityProperty( fennecs.Entity entity, ReadOnlySpan<char> key, ReadOnlySpan<char> value );
+	}
+}
