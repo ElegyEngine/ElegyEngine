82d982d2417690f7132a15a7035902b1082339d3
Admer <49600504+Admer456@users.noreply.github.com>
2024-09-18 04:01:08 +0200

    ECS: add SimpleGenerator

---
+++ src/Generators/Elegy.ECS.Generator/SimpleGenerator.cs
@@ -0,0 +1,35 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Elegy.ECS.Generator
+{
+	public abstract class SimpleGenerator : IIncrementalGenerator
+	{
+		public void Initialize( IncrementalGeneratorInitializationContext context )
+		{
+			var pipeline = context.SyntaxProvider.CreateSyntaxProvider(
+				predicate: ( node, token ) => node is CompilationUnitSyntax,
+				transform: ( syntaxContext, token ) => new SimpleCompilationData( (CompilationUnitSyntax)syntaxContext.Node )
+			);
+
+			context.RegisterSourceOutput(
+				source: pipeline.Collect(),
+				action: ( production, data ) =>
+				{
+					SimpleCompilationData mergedData = new( data[0] );
+					for ( int i = 1; i < data.Length; i++ )
+					{
+						mergedData.Merge( data[i] );
+					}
+
+					Execute( production, mergedData );
+				}
+			);
+		}
+
+		protected abstract void Execute( SourceProductionContext production, SimpleCompilationData data );
+	}
+}
