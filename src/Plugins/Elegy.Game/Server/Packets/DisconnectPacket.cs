e8eaac0446c9472b7dd075bfec8d1a44967a07dc
Admer <49600504+Admer456@users.noreply.github.com>
2024-09-18 04:17:45 +0200

    Add DisconnectPacket stub

---
+++ src/Plugins/Elegy.Game/Server/Packets/DisconnectPacket.cs
@@ -0,0 +1,26 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using Elegy.Common.Interfaces;
+
+namespace Game.Server.Packets
+{
+	/// <summary>
+	/// See <see cref="ServerPacketType.Disconnect"/>.
+	/// </summary>
+	public struct DisconnectPacket : IByteSerialisable
+	{
+		public string Reason { get; set; }
+
+		public void Deserialise( IByteBuffer buffer )
+		{
+			Reason = buffer.ReadStringUtf8( StringLength.Short );
+		}
+
+		public void Serialise( IByteBuffer buffer )
+		{
+
+			buffer.WriteStringUtf8( Reason, StringLength.Medium );
+		}
+	}
+}
