3e52986b1f91065b62812ce9f03089dfbb1219a4
Admer <49600504+Admer456@users.noreply.github.com>
2024-09-18 03:57:15 +0200

    Add DeltaTimer utility

---
+++ src/Core/Elegy.Common/Utilities/DeltaTimer.cs
@@ -0,0 +1,35 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+namespace Elegy.Common.Utilities
+{
+	/// <summary>
+	/// Debounce and timing utilities based on delta time.
+	/// </summary>
+	public struct DeltaTimer
+	{
+		private float mTimer;
+
+		/// <summary>
+		/// Time span of this timer. Once the internal
+		/// clock hits zero, it resets to this.
+		/// </summary>
+		public float Seconds { get; set; }
+
+		public DeltaTimer( float seconds )
+		{
+			Seconds = seconds;
+			mTimer = seconds;
+		}
+
+		public void Update( float delta, Action actionOnTime )
+		{
+			mTimer -= delta;
+			if ( mTimer <= 0.0f )
+			{
+				actionOnTime();
+				mTimer = Seconds;
+			}
+		}
+	}
+}
