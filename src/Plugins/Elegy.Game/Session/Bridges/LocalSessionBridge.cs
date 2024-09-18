03513a8c709b81a306939368263e73fe8cc3679b
Admer <49600504+Admer456@users.noreply.github.com>
2024-09-18 04:06:38 +0200

    Move local & remote session bridge into Session

---
+++ src/Plugins/Elegy.Game/Session/Bridges/LocalSessionBridge.cs
@@ -0,0 +1,63 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using Game.Server.Bridges;
+using Game.Shared;
+using System.Net;
+
+namespace Game.Session.Bridges
+{
+	public class LocalSessionBridge : IServerBridge
+	{
+		private Server.GameServer mServer;
+		private IPAddress Address => IPAddress.IPv6Loopback;
+		private int mClientId = 0;
+
+		private GameSessionState GetConnectionState()
+		{
+			int clientId = mServer.GetClientId( Address );
+			return mServer.Connections[clientId].State;
+		}
+
+		public AssetRegistry AssetRegistry
+			=> mServer.AssetRegistry;
+
+		public GameSessionState ConnectionState
+			=> GetConnectionState();
+
+		public LocalSessionBridge( Server.GameServer server, GameSession session )
+		{
+			mServer = server;
+			mServer.ConnectionStart( Address, new LocalClientBridge( session ) );
+			mClientId = mServer.GetClientId( Address );
+		}
+
+		public void Update( float delta )
+		{
+			// Absolutely nothing here
+		}
+
+		#region Protocol
+		public void SendJoinRequest( IPAddress address )
+			=> mServer.ReceiveJoinRequest( Address );
+
+		public void SendAuthResponse( string? password )
+			=> mServer.ReceiveAuthResponse( mClientId, password );
+
+		public void SendAssetInfoResponse( Span<int> missingAssetIds, Span<int> assetHashes )
+			=> mServer.ReceiveAssetInfoResponse( mClientId, missingAssetIds, assetHashes );
+
+		public void SendSpawnRequest()
+			=> mServer.ReceiveSpawnRequest( mClientId );
+
+		public void SendSpawnComplete()
+			=> mServer.ReceiveSpawnComplete( mClientId );
+
+		public void SendInputPayload( in ClientCommands snapshot )
+			=> mServer.ReceiveInputPayload( mClientId, snapshot );
+
+		public void SendEventPayload( int entityId, int eventId, object[]? args )
+			=> mServer.ReceiveEventPayload( mClientId, entityId, eventId, args );
+		#endregion
+	}
+}
---
+++ src/Plugins/Elegy.Game/Session/Bridges/RemoteSessionBridge.cs
@@ -0,0 +1,105 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using Elegy.ConsoleSystem;
+using Game.Shared;
+using System.Net;
+
+namespace Game.Session.Bridges
+{
+	public class RemoteSessionBridge : IServerBridge
+	{
+		private TaggedLogger mLogger = new( "RemoteSession" );
+		private AssetRegistry mRegistry = new();
+		private GameSession mSession;
+		private GameSessionState mState = GameSessionState.Disconnected;
+		//private Connection mConnection;
+
+		public AssetRegistry AssetRegistry => mRegistry;
+
+		public GameSessionState ConnectionState => mState;
+
+		public RemoteSessionBridge( GameSession session, IPAddress hostAddress )
+		{
+			mSession = session;
+			//mConnection = new( hostAddress );
+		}
+
+		public void Update( float delta )
+		{
+			//var packets = mConnection.PollPackets();
+			//foreach ( var packet in packets )
+			//{
+			//	ProcessPacket( packet.Data );
+			//}
+		}
+
+		private void ProcessPacket( object data )
+		{
+			switch ( data )
+			{
+				// Server is asking us to type a password :)
+				//case Server.Packets.AuthenticationPacket:
+				//	mState = GameSessionState.Authenticating;
+				//	break;
+
+				// Server is sending asset data! Or a URL to them anyway
+				//case Server.Packets.AssetPacket: break;
+
+				// Uh oh we're being kicked
+				case Server.Packets.DisconnectPacket disconnect:
+					mSession.ReceiveDisconnect( disconnect.Reason );
+					mState = GameSessionState.Disconnected;
+					break;
+
+				// Server is sending entity data yaayy :blobjump:
+				//case Server.Packets.GameStatePacket: break;
+
+				// Server is sending entity events let's go
+				//case Server.Packets.GameEventPacket: break;
+
+				default:
+					mLogger.Error( "Received data is not a valid game packet" );
+					break;
+			}
+		}
+
+		#region Protocol
+		public void SendJoinRequest( IPAddress address )
+		{
+			//mConnection.Send( new Packets.JoinRequest() );
+			mState = GameSessionState.Joining;
+		}
+
+		public void SendAuthResponse( string? password )
+		{
+			//mConnection.Send( new Packets.AuthResponse( password ) );
+		}
+
+		public void SendAssetInfoResponse( Span<int> missingAssetIds, Span<int> assetHashes )
+		{
+			//mConnection.Send( new Packets.AssetInfoResponse( Address, missingAssetIds, assetHashes ) );
+		}
+
+		public void SendSpawnRequest()
+		{
+			//mServer.Send( new Packets.SpawnRequest() );
+		}
+
+		public void SendSpawnComplete()
+		{
+			//mServer.Send( new Packets.SpawnComplete() );
+		}
+
+		public void SendInputPayload( in ClientCommands snapshot )
+		{
+			//mConnection.Send( new Packets.InputPayloadPacket( snapshot ) );
+		}
+
+		public void SendEventPayload( int entityId, int eventId, object[]? args )
+		{
+			//mServer.Send( new Packets.EventPayloadPacket( entityId, eventId, args ) );
+		}
+		#endregion
+	}
+}
