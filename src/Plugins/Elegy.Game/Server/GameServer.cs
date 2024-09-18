f7e3b8ab202ad4a385c530f03bbe94b9e86b189f
Admer <49600504+Admer456@users.noreply.github.com>
2024-09-18 04:21:52 +0200

    Server: overhaul
    * Add logging
    * Add Update
    * Add Setup
    * Add SpawnClientEntity
    * Add spawnpoint selection

--- src/Plugins/Elegy.Game/Server/GameServer.cs
+++ src/Plugins/Elegy.Game/Server/GameServer.cs
@@ -2,46 +2,139 @@
 // SPDX-License-Identifier: MIT
 
 using Elegy.Common.Assets;
+using Elegy.Common.Utilities;
+using Elegy.ConsoleSystem;
+
+using Game.Shared;
+using Game.Shared.Components;
+
+using System.Net;
 
 namespace Game.Server
 {
 	public partial class GameServer
 	{
+		private TaggedLogger mLogger = new( "Server" );
+
+		public AssetRegistry AssetRegistry { get; } = new();
+		public EntityWorld EntityWorld { get; }
 		public int MaxPlayers => Connections.Capacity;
 
 		/// <summary>
 		/// How often (per second) to send game state packets to clients.
 		/// </summary>
-		public int GameSnapshotRate => 10;
+		public int GameSnapshotRate { get; set; } = 10;
+		public float GameSnapshotTime => 1.0f / GameSnapshotRate;
 
-		public bool Init()
+		/// <summary>
+		/// How often to update serverside entities.
+		/// </summary>
+		public int ServerUpdateRate { get; set; } = 40;
+		public float ServerUpdateTime => 1.0f / ServerUpdateRate;
+
+		public GameServer( EntityWorld world, int maxPlayers )
 		{
-			if ( !EntityWorld.Init() )
+			EntityWorld = world;
+			Connections = new( maxPlayers );
+		}
+
+		public void Shutdown()
+		{
+		}
+
+		private DeltaTimer mSnapshotTimer = new();
+		private DeltaTimer mUpdateTimer = new();
+
+		public void Update( float delta )
+		{
+			foreach ( var connection in Connections )
 			{
-				return false;
+				if ( connection.State == Session.GameSessionState.Disconnected )
+				{
+					continue;
+				}
+
+				connection.Bridge.Update( delta );
+			}
+
+			// We gotta constantly update these in case they're
+			// changed via a CVar. It's an insignificant perf cost
+			mUpdateTimer.Seconds = ServerUpdateTime;
+			mUpdateTimer.Update( delta, () =>
+			{
+				EntityWorld.Dispatch(
+					new Entity.ServerUpdateEvent( this, ServerUpdateTime ) );
+			} );
+
+			mSnapshotTimer.Seconds = GameSnapshotTime;
+			mSnapshotTimer.Update( delta, () =>
+			{
+				foreach ( var client in Connections )
+				{
+					// Skip the local client
+					if ( IPAddress.IsLoopback( client.Address ) )
+					{
+						continue;
+					}
+
+					//EmitGameState( client );
+				}
+			} );
+		}
+
+		public bool Setup( ElegyMapDocument level )
+		{
+			// Step 1: Create entities
+			foreach ( var entityEntry in level.Entities )
+			{
+				EntityWorld.CreateEntity()
+					.LoadKeyvalues( entityEntry.Attributes )
+					.FinishSpawning();
+			}
+
+			// Step 2: Let entities load all their stuff
+			foreach ( var entity in EntityWorld.AliveEntities )
+			{
+				entity.Dispatch<Entity.SpawnEvent>( new( entity ) );
+			}
+
+			// Step 3: Now that everybody has spawned, do another
+			// run, e.g. accumulating spawnPointEntity links etc.
+			foreach ( var entity in EntityWorld.AliveEntities )
+			{
+				entity.Dispatch<Entity.PostSpawnEvent>( new( entity ) );
 			}
 
 			return true;
 		}
 
-		public void Shutdown()
+		public int SpawnClientEntity( int clientId )
 		{
-			EntityWorld.Shutdown();
+			mLogger.Log( $"Spawning client {clientId}, address '{Connections[clientId].Address}'" );
+
+			Entity playerEntity = EntityWorld
+				.CreateEntity()
+				.With<Player>()
+				.FinishSpawning();
+
+			// TODO: defer this to later when the client fully joins?
+			playerEntity.Dispatch<Entity.SpawnEvent>( new( playerEntity ) );
+
+			SelectSpawnPoint( playerEntity );
+
+			return playerEntity.Id;
 		}
 
-		public void Update( float delta )
+		private void SelectSpawnPoint( Entity player )
 		{
-			// TODO: listen to client connection packets
+			var spawnPointEntity = EntityWorld.Entities[SpawnPoint.SpawnPointIds.Random()];
 
-			EntityWorld.Update( delta );
-		}
+			ref var playerTransform = ref player.Ref<Transform>();
+			ref var spawnPoint = ref spawnPointEntity.Ref<SpawnPoint>();
+			ref var spawnPointTransform = ref spawnPointEntity.Ref<Transform>();
 
-		public bool Setup( int maxPlayers, ElegyMapDocument level )
-		{
-			Connections.Clear();
-
-			// TODO: later we could also obtain the level name etc.
-			return EntityWorld.Setup( level );
+			playerTransform.Position = spawnPointTransform.Position;
+			spawnPoint.PlayerSpawned( new( player.Ref<Player>() ) );
 		}
 	}
 }
