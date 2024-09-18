80931b9fe63cf2711e2ac6af6baa8a47958a261f
Admer <49600504+Admer456@users.noreply.github.com>
2024-09-18 04:16:47 +0200

    Add Breakable stub

---
+++ src/Plugins/Elegy.Game/Shared/Components/Breakable.cs
@@ -0,0 +1,17 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using Elegy.ECS;
+
+namespace Game.Shared.Components
+{
+	[Component]
+	public partial struct Breakable
+	{
+		[Input]
+		public void Break()
+		{
+
+		}
+	}
+}
