ae95aeb33498dc791973b8778286b40d37b01e5f
Admer <49600504+Admer456@users.noreply.github.com>
2024-09-18 04:00:26 +0200

    ECS: add Property attribute

---
+++ src/Core/Elegy.ECS/PropertyAttribute.cs
@@ -0,0 +1,11 @@
+﻿// SPDX-FileCopyrightText: 2024-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+namespace Elegy.ECS
+{
+	[AttributeUsage( AttributeTargets.Property )]
+	public class PropertyAttribute : Attribute
+	{
+
+	}
+}
