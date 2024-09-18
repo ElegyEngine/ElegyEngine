1cc8e7c6cfe331ec2760e6c68f90d20d9730e898
Admer <49600504+Admer456@users.noreply.github.com>
2024-09-18 04:15:10 +0200

    Add Worldspawn

---
+++ src/Plugins/Elegy.Game/Shared/Components/Worldspawn.cs
@@ -0,0 +1,16 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using Elegy.ECS;
+
+namespace Game.Shared.Components
+{
+	[Component]
+	public partial struct Worldspawn
+	{
+		public Worldspawn() { }
+
+		[Property]
+		public string Name { get; set; } = string.Empty;
+	}
+}
