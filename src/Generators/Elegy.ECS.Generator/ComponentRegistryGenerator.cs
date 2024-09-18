b41c2f1311f7fdb475fe096dee22afb6e56d168d
Admer <49600504+Admer456@users.noreply.github.com>
2024-09-18 04:02:17 +0200

    Rewrite ECS generator

---
+++ src/Generators/Elegy.ECS.Generator/ComponentRegistryGenerator.cs
@@ -0,0 +1,62 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using Microsoft.CodeAnalysis;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Elegy.ECS.Generator
+{
+	internal class ComponentDependencyEntry
+	{
+		public ComponentDependencyEntry( SimpleTypeInfo component )
+		{
+			Component = component;
+		}
+
+		public SimpleTypeInfo Component { get; }
+
+		public List<SimpleTypeInfo> Dependencies { get; } = [];
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	[Generator]
+	public partial class ComponentRegistryGenerator : SimpleGenerator
+	{
+		protected override void Execute( SourceProductionContext production, SimpleCompilationData data )
+		{
+			// Save me some typing
+			var types = data.Types;
+
+			// To keep the design as simple as can be, support multiple
+			// component registries that do the exact same thing.
+			var registries = types
+				.Where( t => t.HasAttribute( "GenerateComponentRegistry" ) );
+
+			// Cache all these components early on
+			var components = types
+				.Where( t => t.HasAttribute( "Component" ) );
+
+			// Also cache info for event handling
+			var eventModels = types
+				.Where( t => t.HasAttribute( "EventModel" ) );
+
+			var customEntityProperties = types
+				.Where( t => t.Implements( "IEntityProperty", partial: true ) );
+
+			foreach ( var registry in registries )
+			{
+				GenerateBaseApi( production, registry, components );
+				GenerateComponentKinds( production, registry, components );
+				GeneratePerComponent( production, registry, components, eventModels );
+				GenerateCreate( production, registry, components );
+				GenerateKeyvalue( production, registry, components, customEntityProperties );
+				GenerateEventHandling( production, registry, components );
+				GenerateGroupEventHandling( production, registry, components, eventModels );
+				GenerateNamedEventHandling( production, registry, components );
+			}
+		}
+	}
+}
--- src/Generators/Elegy.ECS.Generator/Utilities.cs
+++ src/Generators/Elegy.ECS.Generator/Utilities.cs
@@ -2,151 +2,60 @@
 // SPDX-License-Identifier: MIT
 
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace Elegy.ECS.Generator
 {
 	public static class Utilities
 	{
-		/// <summary>
-		/// Refer to <see cref="FindAllSymbols(INamespaceOrTypeSymbol, Predicate{ISymbol})"/>.
-		/// </summary>
-		private class PredicateVisitor : SymbolVisitor
+		public class SimpleCompilationVisitor : CSharpSyntaxVisitor
 		{
-			public PredicateVisitor( Predicate<ISymbol> predicate )
+			public SimpleCompilationData Result { get; }
+
+			public SimpleCompilationVisitor( SimpleCompilationData output )
 			{
-				Predicate = predicate;
+				Result = output;
 			}
 
-			public Predicate<ISymbol> Predicate { get; }
-			public List<ISymbol> Result { get; } = new();
-
-			public override void Visit( ISymbol? symbol )
+			public override void VisitCompilationUnit( CompilationUnitSyntax node )
 			{
-				if ( symbol is not null && Predicate( symbol ) )
+				foreach ( var child in node.ChildNodes() )
 				{
-					Result.Add( symbol );
-				}
-
-				base.Visit( symbol );
-			}
-		}
-
-		/// <summary>
-		/// Returns all child and descendant symbols that match the predicate.
-		/// </summary>
-		public static List<ISymbol> FindAllSymbols( INamespaceOrTypeSymbol root, Predicate<ISymbol> filter )
-		{
-			PredicateVisitor visitor = new( filter );
-			root.Accept( visitor );
-			return visitor.Result;
-		}
-
-		/// <summary>
-		/// Refer to <see cref="ForAllSymbols(in IncrementalGeneratorInitializationContext, Action{ISymbol})"/>.
-		/// </summary>
-		private class ActionVisitor : SymbolVisitor
-		{
-			public ActionVisitor( Action<ISymbol> action )
-			{
-				Action = action;
-			}
-
-			public Action<ISymbol> Action { get; }
-
-			public override void Visit( ISymbol? symbol )
-			{
-				if ( symbol is not null )
-				{
-					Action( symbol );
-				}
-
-				base.Visit( symbol );
-			}
-		}
-
-		/// <summary>
-		/// Visits all the symbols with an action.
-		/// </summary>
-		public static void ForAllSymbols( in IncrementalGeneratorInitializationContext context, Action<ISymbol> action )
-		{
-			ForAllCompilations( context, compilation =>
-			{
-				Visit( compilation.GlobalNamespace, action );
-			} );
-		}
-
-		/// <summary>
-		/// Visits the symbol and all child symbols with an action.
-		/// </summary>
-		public static void Visit( ISymbol symbol, Action<ISymbol> action )
-		{
-			symbol.Accept( new ActionVisitor( action ) );
-		}
-
-		/// <summary>
-		/// Visits all the compilations with an action.
-		/// </summary>
-		public static void ForAllCompilations( in IncrementalGeneratorInitializationContext context, Action<Compilation> action )
-		{
-			context.CompilationProvider.Select( ( compilation, token ) =>
-			{
-				action( compilation );
-				return 0;
-			} );
-		}
-
-		/// <summary>
-		/// Returns a fully qualified name of the type marked with
-		/// GenerateComponentRegistry, or <c>null</c> if it cannot be found.
-		/// </summary>
-		public static string? GetComponentRegistryTypeName( in IncrementalGeneratorInitializationContext context )
-		{
-			List<string> results = new();
-
-			// This basically goes through all classes, structs etc.
-			// and checks if they have the GCR attribute
-			ForAllCompilations( context, compilation =>
-			{
-				var symbols = FindAllSymbols(
-					root: compilation.GlobalNamespace,
-					filter: symbol =>
-						symbol is ITypeSymbol
-						&& HasAttribute( symbol, "GenerateComponentRegistryAttribute" )
-				);
-
-				results.AddRange( symbols.Select( s => GetFullName( (ITypeSymbol)s ) ) );
-			} );
-
-			if ( results.Count == 0 )
-			{
-				return null;
-			}
-			// TODO: complain if there's more than 1 result,
-			// only ONE class may be GCR'ed
-			return results[0];
-		}
-
-		public static string? GetComponentRegistryFullName( IEnumerable<ISymbol> symbols )
-		{
-			foreach ( var symbol in symbols )
-			{
-				if ( symbol is null )
-				{
-					continue;
-				}
-
-				if ( HasAttribute( symbol, "GenerateComponentRegistryAttribute" ) )
-				{
-					return GetFullName( (ITypeSymbol)symbol );
+					Visit( child );
 				}
 			}
 
-			return null;
+			public override void VisitNamespaceDeclaration( NamespaceDeclarationSyntax node )
+			{
+				foreach ( var child in node.ChildNodes() )
+				{
+					Visit( child );
+				}
+			}
+
+			private void VisitTypeDeclaration( TypeDeclarationSyntax node )
+			{
+				SimpleTypeInfo info = new( node );
+				Result.TypeDictionary[info.FullName] = info;
+
+				// There may be more types declared inside
+				foreach ( var child in node.ChildNodes() )
+				{
+					Visit( child );
+				}
+			}
+
+			public override void VisitClassDeclaration( ClassDeclarationSyntax node )
+				=> VisitTypeDeclaration( node );
+
+			public override void VisitStructDeclaration( StructDeclarationSyntax node )
+				=> VisitTypeDeclaration( node );
+
+			public override void VisitRecordDeclaration( RecordDeclarationSyntax node )
+				=> VisitTypeDeclaration( node );
 		}
 
 		/// <summary>
@@ -186,100 +95,49 @@ namespace Elegy.ECS.Generator
 		}
 
 		/// <summary>
-		/// Gets the fully qualified name of a symbol.
+		/// Gets the full namespace path to a type.
 		/// </summary>
-		public static string GetFullName( INamespaceOrTypeSymbol symbol )
+		public static string GetNamespace( TypeDeclarationSyntax syntax )
 		{
-			List<string> namespaces = new();
-			INamespaceOrTypeSymbol? containingSymbol = symbol.ContainingType is null
-				? symbol.ContainingNamespace
-				: symbol.ContainingType;
+			string name = syntax.Identifier.ToString();
 
-			while ( containingSymbol is not null )
+			List<string> namespaces = new( 4 );
+			SyntaxNode? containingNode = syntax.Parent;
+			while ( containingNode is not null )
 			{
-				if ( containingSymbol is INamespaceSymbol namespaceSymbol && namespaceSymbol.IsGlobalNamespace )
+				if ( containingNode is NamespaceDeclarationSyntax @namespace )
 				{
-					break;
+					namespaces.Add( @namespace.Name.ToString() );
+				}
+				else if ( containingNode is TypeDeclarationSyntax type )
+				{
+					namespaces.Add( type.Identifier.ToString() );
 				}
 
-				namespaces.Add( containingSymbol.Name );
-
-				if ( containingSymbol.ContainingType is null )
-				{
-					containingSymbol = containingSymbol.ContainingNamespace;
-				}
-				else
-				{
-					containingSymbol = containingSymbol.ContainingType;
-				}
+				containingNode = containingNode.Parent;
 			}
 
 			StringBuilder sb = new( namespaces.Count * 2 + 1 );
-			for ( int i = namespaces.Count - 1; i >= 0; i-- )
+			sb.Append( namespaces[namespaces.Count - 1] );
+
+			for ( int i = namespaces.Count - 2; i >= 0; i-- )
 			{
-				sb.Append( namespaces[i] );
 				sb.Append( '.' );
+				sb.Append( namespaces[i] );
 			}
-			sb.Append( symbol.Name );
 
 			return sb.ToString();
 		}
 
 		/// <summary>
-		/// Whether or not a symbol is marked with an attribute, at least partially.
+		/// Obtains a generic type argument from a type name. Only supports one type parametre!
 		/// </summary>
-		public static bool HasAttribute( ISymbol symbol, string attributeName, bool partial = false )
+		public static string GetGenericArgument( string typeName )
 		{
-			foreach ( var attributeData in symbol.GetAttributes() )
-			{
-				if ( attributeData.AttributeClass is null )
-				{
-					continue;
-				}
+			int leftArrow = typeName.IndexOf( '<' ) + 1;
+			int rightArrow = typeName.LastIndexOf( '>' );
 
-				string actualName = GetFullName( attributeData.AttributeClass );
-
-				if ( actualName == attributeName )
-				{
-					return true;
-				}
-
-				if ( partial && actualName.Contains( attributeName ) )
-				{
-					return true;
-				}
-			}
-
-			return false;
-		}
-
-		public static void SetupSymbolBasedGenerator( in IncrementalGeneratorInitializationContext context, bool implementationOutput,
-			Action<SourceProductionContext, IEnumerable<ISymbol>> action )
-		{
-			var pipeline = context.SyntaxProvider.CreateSyntaxProvider(
-				predicate: ( node, token )
-					=> node is TypeDeclarationSyntax,
-
-				transform: ( syntaxContext, token )
-					=> syntaxContext.SemanticModel.GetSymbolInfo( syntaxContext.Node ).Symbol
-			);
-
-			if ( implementationOutput )
-			{
-				context.RegisterImplementationSourceOutput( pipeline.Collect(), ( productionContext, data ) =>
-				{
-					IEnumerable<ISymbol> dataNotNull = data.Where( s => s is not null );
-					action( productionContext, dataNotNull );
-				} );
-			}
-			else
-			{
-				context.RegisterSourceOutput( pipeline.Collect(), ( productionContext, data ) =>
-				{
-					IEnumerable<ISymbol> dataNotNull = data.Where( s => s is not null );
-					action( productionContext, dataNotNull );
-				} );
-			}
+			return typeName.Substring( leftArrow, rightArrow - leftArrow );
 		}
 	}
 }
