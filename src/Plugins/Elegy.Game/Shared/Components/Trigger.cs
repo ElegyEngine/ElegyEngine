d6af698f8a78a326adbfaf3a9d2915158e462855
Admer <49600504+Admer456@users.noreply.github.com>
2024-09-18 04:15:17 +0200

    Add Trigger

---
+++ src/Plugins/Elegy.Game/Shared/Components/Trigger.cs
@@ -0,0 +1,29 @@
+﻿// SPDX-FileCopyrightText: 2022-present Elegy Engine contributors
+// SPDX-License-Identifier: MIT
+
+using Elegy.ECS;
+
+namespace Game.Shared.Components
+{
+	[Component]
+	//[Requires<Collider>] // we don't have those yet
+	public partial struct Trigger
+	{
+		[Event]
+		public void Touch( Entity.TouchEvent data )
+		{
+			if ( data.Other.Has<Player>() )
+			{
+				OnPlayerEnter.Fire();
+			}
+
+			OnEnter.Fire();
+		}
+
+		[Property]
+		public EntityOutput OnPlayerEnter { get; set; }
+
+		[Property]
+		public EntityOutput OnEnter { get; set; }
+	}
+}
