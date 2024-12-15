87c69cbf1b5cca44f27324d2e16c29623e4911ed
Admer <49600504+Admer456@users.noreply.github.com>
2024-12-15 23:53:53 +0100

    Mesh: minor reformat

--- src/Core/Elegy.Common/Assets/MeshData/Mesh.cs
+++ src/Core/Elegy.Common/Assets/MeshData/Mesh.cs
@@ -45,34 +45,49 @@ namespace Elegy.Common.Assets.MeshData
 
 		/// <summary> Vertex positions. </summary>
 		public Vector3[] Positions { get; set; } = Array.Empty<Vector3>();
+
 		/// <summary> Vertex positions but 2D. </summary>
 		public Vector2[] Positions2D { get; set; } = Array.Empty<Vector2>();
+
 		/// <summary> Vertex normals. </summary>
 		public Vector3[] Normals { get; set; } = Array.Empty<Vector3>();
+
 		/// <summary> Vertex normals but 2D. </summary>
 		public Vector2[] Normals2D { get; set; } = Array.Empty<Vector2>();
+
 		/// <summary> Vertex tangents. </summary>
 		public Vector4[] Tangents { get; set; } = Array.Empty<Vector4>();
+
 		/// <summary> Vertex texture coordinates, channel 0. </summary>
 		public Vector2[] Uv0 { get; set; } = Array.Empty<Vector2>();
+
 		/// <summary> Vertex texture coordinates, channel 1. </summary>
 		public Vector2[] Uv1 { get; set; } = Array.Empty<Vector2>();
+
 		/// <summary> Vertex texture coordinates, channel 2. </summary>
 		public Vector2[] Uv2 { get; set; } = Array.Empty<Vector2>();
+
 		/// <summary> Vertex texture coordinates, channel 3. </summary>
 		public Vector2[] Uv3 { get; set; } = Array.Empty<Vector2>();
+
 		/// <summary> Vertex colours, channel 0. </summary>
 		public Vector4B[] Color0 { get; set; } = Array.Empty<Vector4B>();
+
 		/// <summary> Vertex colours, channel 1. </summary>
 		public Vector4B[] Color1 { get; set; } = Array.Empty<Vector4B>();
+
 		/// <summary> Vertex colours, channel 2. </summary>
 		public Vector4B[] Color2 { get; set; } = Array.Empty<Vector4B>();
+
 		/// <summary> Vertex colours, channel 3. </summary>
 		public Vector4B[] Color3 { get; set; } = Array.Empty<Vector4B>();
+
 		/// <summary> Vertex bone indices. </summary>
 		public Vector4B[] BoneIndices { get; set; } = Array.Empty<Vector4B>();
+
 		/// <summary> Normalised vertex bone weights. </summary>
 		public Vector4[] BoneWeights { get; set; } = Array.Empty<Vector4>();
+
 		/// <summary> Vertex indices. </summary>
 		public uint[] Indices { get; set; } = Array.Empty<uint>();
 
@@ -110,8 +125,8 @@ namespace Elegy.Common.Assets.MeshData
 
 		/// <summary> Transforms a sequence of vertex data into another format, e.g. float3 into byte4. </summary>
 		public static TDestination[] Transform<TSource, TDestination>( IList<TSource> source, Func<TSource, TDestination> transform )
-			where TSource: unmanaged
-			where TDestination: unmanaged
+			where TSource : unmanaged
+			where TDestination : unmanaged
 		{
 			// Say no to LINQ !!!
 			TDestination[] result = new TDestination[source.Count];
