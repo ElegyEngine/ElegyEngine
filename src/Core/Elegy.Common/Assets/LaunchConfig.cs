6e81d63c9b3b775ba14181161e0e7fa5dcab58b3
Admer <49600504+Admer456@users.noreply.github.com>
2024-12-17 23:18:22 +0100

    LaunchConfig: minor cleanup

--- src/Core/Elegy.Common/Assets/LaunchConfig.cs
+++ src/Core/Elegy.Common/Assets/LaunchConfig.cs
@@ -9,21 +9,21 @@ namespace Elegy.Common.Assets
 	public class LaunchConfig
 	{
 		/// <summary>Path to engine configuration. Overrides <see cref="Engine"/>.</summary>
-		public string? EngineConfigName { get; set; } = null;
+		public string? EngineConfigName { get; set; }
 
 		/// <summary>Engine configuration. By default will load 'engineConfig.json'.</summary>
 		public EngineConfig Engine { get; set; } = new();
 
 		/// <summary>Commandline arguments.</summary>
-		public string[] Args { get; set; } = Array.Empty<string>();
+		public string[] Args { get; set; } = [];
 
 		/// <summary>Console frontends to be loaded up front.</summary>
-		public IPlugin[]? ConsoleFrontends { get; set; } = null;
+		public IPlugin[]? ConsoleFrontends { get; set; }
 
 		/// <summary>Whether to create a main window or not at the start.</summary>
 		public bool WithMainWindow { get; set; } = true;
 
 		/// <summary>If set to <c>true</c>, will not load application plugins.</summary>
-		public bool ToolMode { get; set; } = false;
+		public bool ToolMode { get; set; }
 	}
 }
