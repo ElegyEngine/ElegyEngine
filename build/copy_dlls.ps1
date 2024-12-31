78693b24783aa3ec9700447bb099137242a669ed
Admer <49600504+Admer456@users.noreply.github.com>
2024-12-31 19:57:08 +0100

    Copy BepuPhysics DLLs as well

--- build/copy_dlls.ps1
+++ build/copy_dlls.ps1
@@ -30,9 +30,10 @@ if ( $plugin = "none" )
 	## Copy the plugin DLLs without their dependencies
 	Copy-Item "$input_dir/src/Plugins/Elegy.DevConsole/bin/$build_config/net8.0/Elegy.DevConsole.*"			-Destination "$output_dir/engine/plugins/DevConsole"
 	Copy-Item "$input_dir/src/Plugins/Elegy.Game/bin/$build_config/net8.0/Game.*"							-Destination "$output_dir/game/plugins/Game"
-	Copy-Item "$input_dir/src/Plugins/Elegy.Game/bin/$build_config/net8.0/Elegy.RenderWorld.*" 				-Destination "$output_dir/game/plugins/Game"
+	Copy-Item "$input_dir/src/Plugins/Elegy.Game/bin/$build_config/net8.0/Bepu*"							-Destination "$output_dir/game/plugins/Game"
 	Copy-Item "$input_dir/src/Plugins/Elegy.Game/bin/$build_config/net8.0/fennecs.*" 						-Destination "$output_dir/game/plugins/Game"
 	Copy-Item "$input_dir/src/Plugins/Elegy.Game/bin/$build_config/net8.0/Elegy.ECS.*" 						-Destination "$output_dir/game/plugins/Game"
+	Copy-Item "$input_dir/src/Plugins/Elegy.Game/bin/$build_config/net8.0/Elegy.RenderWorld.*" 				-Destination "$output_dir/game/plugins/Game"
 }
 else
 {
