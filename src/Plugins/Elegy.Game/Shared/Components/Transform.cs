e4e04c8ac31e040d191bf279f812c1c05b1bf598
Admer <49600504+Admer456@users.noreply.github.com>
2024-12-31 20:09:23 +0100

    Game: update Transform

--- src/Plugins/Elegy.Game/Shared/Components/Transform.cs
+++ src/Plugins/Elegy.Game/Shared/Components/Transform.cs
@@ -8,7 +8,33 @@ namespace Game.Shared.Components
 	[Component]
 	public partial struct Transform
 	{
-		public Vector3 Position { get; set; }
-		public Quaternion Orientation { get; set; }
+		public Transform()
+		{
+		}
+
+		private Vector3 mPosition = Vector3.Zero;
+		private Quaternion mOrientation = Quaternion.Identity;
+
+		public bool TransformDirty { get; set; }
+
+		public Vector3 Position
+		{
+			get => mPosition;
+			set
+			{
+				mPosition = value;
+				TransformDirty = true;
+			}
+		}
+
+		public Quaternion Orientation
+		{
+			get => mOrientation;
+			set
+			{
+				mOrientation = value;
+				TransformDirty = true;
+			}
+		}
 	}
 }
