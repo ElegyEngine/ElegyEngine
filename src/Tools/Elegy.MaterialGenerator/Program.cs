3afbf366a10e2e6bc9e32e006294d00f63bc2356
Admer <49600504+Admer456@users.noreply.github.com>
2025-01-25 21:54:36 +0100

    Update MaterialGenerator
    * Points to game folder now
    * Has more intelligent validation
    * Materials are now in 'materials'
    * Textures are now in 'textures', i.e.
    * no longer as a subfolder of 'materials'

--- src/Tools/Elegy.MaterialGenerator/Program.cs
+++ src/Tools/Elegy.MaterialGenerator/Program.cs
@@ -12,8 +12,8 @@ namespace Elegy.MaterialGenerator
 			if ( args.Length == 0 )
 			{
 				Console.WriteLine( "Oh hey! I basically scan texture files and generate Elegy materials for them. :)" );
-				Console.WriteLine( "In order to use me, provide a path to your textures folder, for example:" );
-				Console.WriteLine( "C:/MyGame/game/materials/textures" );
+				Console.WriteLine( "In order to use me, provide a path to your game folder, for example:" );
+				Console.WriteLine( "./Elegy.MaterialGenerator \"C:/MyGame/game\"" );
 				return;
 			}
 
@@ -25,11 +25,23 @@ namespace Elegy.MaterialGenerator
 				return;
 			}
 
+			string rootPath = args[0].Replace( '\\', '/' );
+			string materialsPath = Path.Combine( rootPath, "materials" ).Replace( '\\', '/' );
+			string texturesPath = Path.Combine( rootPath, "textures" ).Replace( '\\', '/' );
+
+			if ( !ValidatePath( rootPath, materialsPath ) )
+			{
+				return;
+			}
+
+			if ( !ValidatePath( rootPath, texturesPath ) )
+			{
+				return;
+			}
+
 			do
 			{
-				string rootPath = args[0].Replace( '\\', '/' );
-
-				var entries = Directory.GetFileSystemEntries( args[0], "*", SearchOption.AllDirectories );
+				var entries = Directory.GetFileSystemEntries( texturesPath, "*", SearchOption.AllDirectories );
 				foreach ( var entry in entries )
 				{
 					if ( !Directory.Exists( entry ) )
@@ -37,17 +49,18 @@ namespace Elegy.MaterialGenerator
 						continue;
 					}
 
-					string materialDirectoryRelative = Path.GetRelativePath( args[0], entry ).Replace( '\\', '/' );
-					if ( materialDirectoryRelative.StartsWith( "tools" ) )
+					string textureDirectory = Path.GetRelativePath( texturesPath, entry ).Replace( '\\', '/' );
+					if ( textureDirectory.StartsWith( "tools" ) )
 					{
-						Console.WriteLine( $"Skipping '{materialDirectoryRelative}' because it contains tool textures - you gotta do those manually." );
+						Console.WriteLine(
+							$"Skipping '{textureDirectory}' because it contains tool textures - you gotta do those manually." );
 						continue;
 					}
 
 					StringBuilder sb = new();
-					Console.Write( $"Processing textures for: {materialDirectoryRelative}" );
+					Console.WriteLine( $"Processing textures for: {textureDirectory}" );
 
-					var imageEntries = Directory.GetFiles( Path.Combine( rootPath, materialDirectoryRelative ), "*" );
+					var imageEntries = Directory.GetFiles( Path.Combine( texturesPath, textureDirectory ), "*" );
 					foreach ( var imageEntry in imageEntries )
 					{
 						string imagePath = Path.GetRelativePath( entry, imageEntry );
@@ -55,28 +68,70 @@ namespace Elegy.MaterialGenerator
 						Console.WriteLine( $"  * {imagePathNoExt}" );
 						sb.AppendLine(
 							$$"""
-							materials/{{materialDirectoryRelative}}/{{imagePathNoExt}}
-							{
-								materialTemplate Standard
-								{
-									map materials/textures/{{materialDirectoryRelative}}/{{imagePath}}
-								}
-							}
+							  materials/{{textureDirectory}}/{{imagePathNoExt}}
+							  {
+							  	materialTemplate Standard
+							  	{
+							  		map textures/{{textureDirectory}}/{{imagePath}}
+							  	}
+							  }
 
-							""" );
+							  """ );
 					}
 
-					string materialFileName = materialDirectoryRelative
+					string materialFileName = textureDirectory
 						.Replace( '/', '_' )
 						.Replace( ' ', '_' );
 
-					File.WriteAllText( $"{args[0]}/../mat_{materialFileName}.shader", sb.ToString() );
+					File.WriteAllText( $"{materialsPath}/mat_{materialFileName}.shader", sb.ToString() );
 
 					Console.WriteLine( " - done!" );
 				}
 
 				Console.WriteLine( "Done! Press any key to repeat all this, or press ESC if you're done." );
 			} while ( Console.ReadKey().Key != ConsoleKey.Escape );
+		}
+
+		public static bool ValidatePath( string root, string path )
+		{
+			if ( Directory.Exists( path ) )
+			{
+				return true;
+			}
+
+			Console.WriteLine( "ERROR: The following path doesn't exist:" );
+			Console.WriteLine( $"'{path}'" );
+
+			foreach ( var file in Directory.GetFiles( root ) )
+			{
+				if ( Path.GetExtension( file ) != ".exe" )
+				{
+					continue;
+				}
+				
+				string filename = Path.GetRelativePath( root, file );
+				
+				if ( filename.Contains( "Elegy" ) || filename.Contains( "Launcher" ) )
+				{
+					Console.WriteLine( $"One or more engine launchers detected ({filename})." );
+					Console.WriteLine( "This means you are in the game's root directory." );
+					Console.WriteLine( "You most likely need to point to a subfolder from here, such as:" );
+					foreach ( var folder in Directory.GetDirectories( root ) )
+					{
+						string folderRelative = Path.GetRelativePath( root, folder );
+						if ( folderRelative is "engine" or "runtimes" )
+						{
+							continue;
+						}
+						
+						Console.WriteLine( $"  * {folderRelative}" );
+					}
+
+					break;
+				}
+			}
+
+			return false;
 		}
 	}
 }
