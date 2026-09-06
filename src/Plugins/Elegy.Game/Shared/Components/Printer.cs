355db9b6ded28d90c27204504bcc97a706554bfd
Admer456 <admer456@gmail.com>
2026-09-06 04:22:23 +0200

    Game: add Printer component

---
+++ src/Plugins/Elegy.Game/Shared/Components/Printer.cs
@@ -0,0 +1,18 @@
+
+using Elegy.ECS;
+
+namespace Game.Shared.Components
+{
+	[Component]
+	public partial struct Printer
+	{
+		[Property]
+		public string Message { get; set; }
+
+		[Input]
+		public void Print()
+		{
+			Console.Submit( Message );
+		}
+	}
+}
