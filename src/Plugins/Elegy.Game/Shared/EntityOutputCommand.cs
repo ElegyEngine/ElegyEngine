806214030afc86348944de354609da2d0355d159
Admer456 <admer456@gmail.com>
2026-09-09 04:28:28 +0200

    A little note for output commands

--- src/Plugins/Elegy.Game/Shared/EntityOutputCommand.cs
+++ src/Plugins/Elegy.Game/Shared/EntityOutputCommand.cs
@@ -12,5 +12,16 @@ namespace Game.Shared
 		public Entity Receiver; // 64
 		public long ExecutionTime; // 64
 		public long InputId; // 64, EntityUtilities.ComponentInput
+
+		/* Alternative structure:
+		 * Sender - i32 id (4 bytes)
+		 * Receiver - i32 id (4 bytes)
+		 *            ^ storing the whole ECS entity is faster but eh
+		 * Exectime + input:
+		 * - Exectime - u48 usec (6 bytes)
+		 * - Input - u16 id (2 bytes)
+		 * This reduces it to a total of 16 bytes. We can then add another 16 bytes of
+		 * data at the end, which could be used for parametres and such.
+		 */
 	}
 }
